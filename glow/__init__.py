e6967b1a90db23059b067a71c195c87f443b9ea9
Emmanuel Turlay <emmanuel@sematic.ai>
2022-04-22 09:29:48 -0700

    public api

---
+++ glow/__init__.py
@@ -0,0 +1,6 @@
+"""
+Glow Public API
+"""
+from glow.calculator import calculator  # noqa: F401
+from glow.resolver import Resolver  # noqa: F401
+from glow.resolvers.local_resolver import LocalResolver  # noqa: F401
---
+++ glow/tests/test_init.py
@@ -0,0 +1,10 @@
+"""
+Testing public API imports
+"""
+
+# Glow
+from glow import calculator, LocalResolver, Resolver  # noqa: F401
+
+
+def test_pass():
+    assert True
