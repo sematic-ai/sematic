ed3c565861dcdb804fef86954ef35af2fe2c0ced
Emmanuel Turlay <emmanuel@sematic.ai>
2022-04-29 23:07:31 -0700

    Creating runs in resolver

--- glow/db/schema.sql
+++ glow/db/schema.sql
@@ -1,41 +1,19 @@
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
+CREATE TABLE IF NOT EXISTS "schema_migrations" (version varchar(255) primary key);
+CREATE TABLE runs (
+    id character(32) NOT NULL,
+    future_state TEXT NOT NULL,
+    name TEXT,
+    calculator_path TEXT,
+    created_at timestamp without time zone,
+    updated_at timestamp without time zone,
+    started_at timestamp without time zone,
+    ended_at timestamp without time zone,
+    resolved_at timestamp without time zone,
+    failed_at timestamp without time zone,
+    parent_id character(32),
 
-SET default_tablespace = '';
-
-SET default_table_access_method = heap;
-
---
--- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.schema_migrations (
-    version character varying(255) NOT NULL
+    PRIMARY KEY (id)
 );
-
-
---
--- Name: schema_migrations schema_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.schema_migrations
-    ADD CONSTRAINT schema_migrations_pkey PRIMARY KEY (version);
-
-
---
--- PostgreSQL database dump complete
---
-
-
---
 -- Dbmate schema migrations
---
-
+INSERT INTO "schema_migrations" (version) VALUES
+  ('20220424062956');
