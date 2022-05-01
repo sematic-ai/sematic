3bdfade20d1f0a6b6096905149d0f0f1b3976cc1
Jai Chopra <jaichopra@gmail.com>
2022-05-01 14:41:34 -0700

    data table

--- web/packages/app/src/App.tsx
+++ web/packages/app/src/App.tsx
@@ -1,19 +1,13 @@
 import { APP_TITLE } from '@glow-web/common';
 import * as React from 'react';
+import Dashboard from './Dashboard';
 
 export function App(): React.ReactElement {
   const [count, setCount] = React.useState(0);
 
   return (
     <div>
-      <h1>Welcome on {APP_TITLE}!</h1>
-      <p>
-        This is the main page of our application where you can confirm that it
-        is dynamic by clicking the button below.
-      </p>
-
-      <p>Current count: {count}</p>
-      <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
+      <Dashboard />
     </div>
   );
 }
---
+++ web/packages/app/src/listItems.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+
+export const mainListItems = (
+  <React.Fragment>
+    <ListItemButton>
+      <ListItemIcon>
+        <DashboardIcon />
+      </ListItemIcon>
+      <ListItemText primary="Runs" />
+    </ListItemButton>
+  </React.Fragment>
+);
+
