8b88ec8da1ddf8f3ef9f793dfd473c87986d94db
Jai Chopra <jaichopra@gmail.com>
2022-05-02 21:13:36 -0700

    minor changes

---
+++ web/packages/app/src/components/GlowChip/GlowChipColors.tsx
@@ -0,0 +1,44 @@
+const GlowChipColors = [
+    {
+        name: "Yellow",
+        hexCode: "#fbd673"
+    },
+    {
+        name: "Orange",
+        hexCode: "#fea885"
+    },
+    {
+        name: "Red",
+        hexCode: "#ff9eb5"
+    },
+    {
+        name: "Pink",
+        hexCode: "#f7a0e1"
+    },
+    {
+        name: "Purple",
+        hexCode: "#ceb3fb"
+    },
+    {
+        name: "Blue",
+        hexCode: "#9dc7fe"
+    },
+    {
+        name: "Cyan",
+        hexCode: "#71d4f1"
+    },
+    {
+        name: "Teal",
+        hexCode: "#74dec4"
+    },
+    {
+        name: "Green",
+        hexCode: "#98db8e"
+    },
+    {
+        name: "Grey",
+        hexCode: "#cccccc"
+    }
+];
+
+export default GlowChipColors;
