781bacaa3edab0f5c9156f603d6c2bc9ef2dd2e1
Jai Chopra <jaichopra@gmail.com>
2022-05-02 17:31:13 -0700

    clean up code

--- web/packages/app/src/Title.tsx
+++ web/packages/app/src/Title.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import Typography from '@mui/material/Typography';
+import React from "react";
+import Typography from "@mui/material/Typography";
 
 interface TitleProps {
   children?: React.ReactNode;
