9ebd5b9fac65e6f8dc990e080536d16bd828f8c9
Jai Chopra <jaichopra@gmail.com>
2022-05-03 11:02:42 -0700

    setup react router and use layouts appropriately

--- web/packages/app/src/index.tsx
+++ web/packages/app/src/index.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { BrowserRouter } from "react-router-dom";
+
 import { App } from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById("root")
+  );
+  
