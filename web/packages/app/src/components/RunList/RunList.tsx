6e77946bde0dc2b32c5a9912d72d8f0b0549b35a
Jai Chopra <jaichopra@gmail.com>
2022-05-02 17:31:13 -0700

    clean up code

--- web/packages/app/src/components/RunList/RunList.tsx
+++ web/packages/app/src/components/RunList/RunList.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import * as React from "react";
 import styled from "@emotion/styled";
 import {
   Link,
@@ -10,7 +10,7 @@ import {
   Alert as MuiAlert,
   Chip as MuiChip,
 } from "@mui/material";
-import { DataGrid, GridColDef, GridToolbar } from '@mui/x-data-grid';
+import { DataGrid, GridColDef, GridToolbar } from "@mui/x-data-grid";
 import { spacing, sizing } from "@mui/system";
 
 const Card = styled(MuiCard)(spacing);
@@ -24,29 +24,29 @@ const Chip = styled(MuiChip)(spacing);
 const Alert = styled(MuiAlert)(sizing);
 
 const columns: GridColDef[] = [
-  { field: 'id', headerName: 'Run ID', width: 90 },
+  { field: "id", headerName: "Run ID", width: 90 },
   {
-    field: 'createdAt',
-    headerName: 'Created at',
+    field: "createdAt",
+    headerName: "Created at",
     editable: false,
     flex: 1,
   },
   {
-    field: 'blockName',
-    headerName: 'Name',
+    field: "blockName",
+    headerName: "Name",
     editable: false,
     flex: 1,
     renderCell: (props) => {
       return (
         <React.Fragment>
-          <Link href="localhost:3000/">{props.row.blockName}</Link>
+          <Link href="/">{props.row.blockName}</Link>
         </React.Fragment>
       );
     }
   },
   {
-    field: 'tags',
-    headerName: 'Tags',
+    field: "tags",
+    headerName: "Tags",
     editable: false,
     flex: 1,
     renderCell: (props) => {
@@ -64,12 +64,12 @@ const columns: GridColDef[] = [
     }
   },
   {
-    field: 'status',
-    headerName: 'Status',
-    description: 'This column has a value getter and is not sortable.',
+    field: "status",
+    headerName: "Status",
+    description: "This column has a value getter and is not sortable.",
     flex: 1,
     renderCell: (props: any) => {
-      if (props.row.status === 'success') {
+      if (props.row.status === "success") {
         return (
           <React.Fragment>
             <Alert severity="success" width={"100%"}>
@@ -103,35 +103,35 @@ function createData(
 const rows = [
   createData(
     0,
-    '16 Mar, 2019',
-    'Auto Featurization',
-    ['experiment-blahsd', 'random-tag'],
-    'success'
+    "16 Mar, 2019",
+    "Auto Featurization",
+    ["experiment-blahsd", "random-tag"],
+    "success"
   ),
   createData(
     1,
-    '16 Mar, 2019',
-    'Train model',
-    ['experiment-bler', 'randfdsf'],
-    'success'
+    "16 Mar, 2019",
+    "Train model",
+    ["experiment-bler", "randfdsf"],
+    "success"
   ),
   createData(
     2,
-    '16 Mar, 2019',
-    'Train model',
-    ['experiment-bler', '2daf-tags', 'dadceef'],
-    'failed'
+    "16 Mar, 2019",
+    "Train model",
+    ["experiment-bler", "2daf-tags", "dadceef"],
+    "failed"
   ),
   createData(
     3,
-    '15 Mar, 2019',
-    'Evaluate model',
-    ['ddasgfa'],
-    'success'
+    "15 Mar, 2019",
+    "Evaluate model",
+    ["ddasgfa"],
+    "success"
   ),
 ];
 
-export default function RunsList() {
+export default function RunList() {
   return (
     <Card mb={6}>
       <CardContent pb={1}>
