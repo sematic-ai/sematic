47d18d582fe24940546d32030bed7cd0b9bdbe61
Emmanuel Turlay <emmanuel@sematic.ai>
2022-05-08 21:16:25 -0700

    Docs

--- glow/types/types/float.py
+++ glow/types/types/float.py
@@ -8,6 +8,11 @@ from glow.types.registry import register_can_cast, register_safe_cast
 
 @register_can_cast(float)
 def can_cast_type(type_: type, _) -> typing.Tuple[bool, typing.Optional[str]]:
+    """
+    Type casting logic for `float`.
+
+    Only subclasses of `numbers.Real` can cast to `float`.
+    """
     if issubclass(type_, numbers.Real):
         return True, None
 
@@ -16,6 +21,11 @@ def can_cast_type(type_: type, _) -> typing.Tuple[bool, typing.Optional[str]]:
 
 @register_safe_cast(float)
 def safe_cast(value: typing.Any, _) -> typing.Tuple[typing.Any, typing.Optional[str]]:
+    """
+    Value casting logic for `float`.
+
+    The native Python logic is preserved.
+    """
     try:
         return float(value), None
     except ValueError as exception:
--- glow/types/types/integer.py
+++ glow/types/types/integer.py
@@ -8,6 +8,11 @@ from glow.types.registry import register_can_cast, register_safe_cast
 
 @register_can_cast(int)
 def can_cast_type(type_: type, _) -> typing.Tuple[bool, typing.Optional[str]]:
+    """
+    Type casting logic for `int`.
+
+    Only subclasses of `numbers.Real` can cast to `int`.
+    """
     if issubclass(type_, numbers.Real):
         return True, None
 
@@ -16,6 +21,11 @@ def can_cast_type(type_: type, _) -> typing.Tuple[bool, typing.Optional[str]]:
 
 @register_safe_cast(int)
 def safe_cast(value: typing.Any, _) -> typing.Tuple[typing.Any, typing.Optional[str]]:
+    """
+    Value casting logic for `int`.
+
+    The native Python logic is preserved.
+    """
     try:
         return int(value), None
     except ValueError as exception:
