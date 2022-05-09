5d1e961bfbda4a6f7b57f5f45d622cefffde2c49
Emmanuel Turlay <emmanuel@sematic.ai>
2022-05-08 21:57:54 -0700

    get_run query

--- glow/db/queries.py
+++ glow/db/queries.py
@@ -25,6 +25,11 @@ def create_run(run: Run) -> Run:
     """
     Create a new run.
 
+    Parameters
+    ----------
+    run : Run
+        Run to persist.
+
     Returns
     -------
     Run
@@ -36,3 +41,21 @@ def create_run(run: Run) -> Run:
         session.refresh(run)
 
     return run
+
+
+def get_run(run_id: str) -> Run:
+    """
+    Get a run from the database.
+
+    Parameters
+    ----------
+    run_id : str
+        ID of run to retrieve.
+
+    Returns
+    -------
+    Run
+        Fetched run
+    """
+    with db().get_session() as session:
+        return session.query(Run).filter(Run.id == run_id).one()
--- glow/db/tests/test_queries.py
+++ glow/db/tests/test_queries.py
@@ -1,7 +1,7 @@
 # Glow
-from glow.db.queries import count_runs, create_run
+from glow.db.queries import count_runs, create_run, get_run
 from glow.db.models.run import Run
-from glow.db.tests.fixtures import test_db, run  # noqa: F401
+from glow.db.tests.fixtures import test_db, run, persisted_run  # noqa: F401
 
 
 def test_count_runs(test_db, run: Run):  # noqa: F811
@@ -12,7 +12,13 @@ def test_count_runs(test_db, run: Run):  # noqa: F811
 
 def test_create_run(test_db, run: Run):  # noqa: F811
     assert run.created_at is None
-    persisted_run = create_run(run)
-    assert persisted_run == run
+    created_run = create_run(run)
+    assert created_run == run
     assert run.created_at is not None
     assert run.updated_at is not None
+
+
+def test_get_run(test_db, persisted_run: Run):  # noqa: F811
+    fetched_run = get_run(persisted_run.id)
+
+    assert fetched_run.id == persisted_run.id
