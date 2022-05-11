81810d27af04ab88201170d4b352b4cbbedfc3e1
Emmanuel Turlay <emmanuel@sematic.ai>
2022-05-11 08:52:54 -0700

    flake8 and mypy

--- glow/api/endpoints/tests/test_runs.py
+++ glow/api/endpoints/tests/test_runs.py
@@ -1,4 +1,3 @@
-# Standard library
 import typing
 
 # Third-party
@@ -31,20 +30,19 @@ def test_list_runs(test_client: flask.testing.FlaskClient):  # noqa: F811
 
     results = test_client.get("/api/v1/runs?limit=3")
 
-    assert len(results.json["next_page_url"]) > 0
-    assert len(results.json["next_cursor"]) > 0
-    assert results.json["after_cursor_count"] == len(created_runs)
-    assert results.json["content"] == [
-        run.to_json_encodable() for run in created_runs[:3]
-    ]
+    payload = results.json
+    payload = typing.cast(typing.Dict[str, typing.Any], payload)
 
-    next_page_url = results.json["next_page_url"]
+    assert len(payload["next_page_url"]) > 0
+    assert len(payload["next_cursor"]) > 0
+    assert payload["after_cursor_count"] == len(created_runs)
+    assert payload["content"] == [run.to_json_encodable() for run in created_runs[:3]]
+
+    next_page_url = payload["next_page_url"]
     next_page_url = next_page_url.split("localhost")[1]
 
     results = test_client.get(next_page_url)
-    assert results.json["next_page_url"] is None
-    assert results.json["next_cursor"] is None
-    assert results.json["after_cursor_count"] == 2
-    assert results.json["content"] == [
-        run.to_json_encodable() for run in created_runs[3:]
-    ]
+    assert payload["next_page_url"] is None
+    assert payload["next_cursor"] is None
+    assert payload["after_cursor_count"] == 2
+    assert payload["content"] == [run.to_json_encodable() for run in created_runs[3:]]
