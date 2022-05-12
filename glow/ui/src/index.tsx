ed8f1084f8d7f8b59ab24489a0d10129061739af
Emmanuel Turlay <emmanuel@sematic.ai>
2022-05-12 15:38:24 -0700

    fix mypy

--- glow/ui/src/index.tsx
+++ glow/ui/src/index.tsx
@@ -6,6 +6,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import './index.css';
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
