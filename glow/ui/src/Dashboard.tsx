27de163dc4ac6c8ec86b457569c4f3421f9a7d18
Emmanuel Turlay <emmanuel@sematic.ai>
2022-05-13 14:27:22 -0700

    formatting

--- glow/ui/src/Dashboard.tsx
+++ glow/ui/src/Dashboard.tsx
@@ -4,7 +4,7 @@ import TableRow from "@mui/material/TableRow";
 import { useEffect, useState } from "react";
 import RunList from "./components/RunList";
 import { Run } from "./Models";
-import Link from '@mui/material/Link';
+import Link from "@mui/material/Link";
 import { RunListPayload } from "./Payloads";
 import RunStateChip from "./components/RunStateChip";
 import CircleOutlined from "@mui/icons-material/CircleOutlined";
@@ -12,21 +12,18 @@ import CircleOutlined from "@mui/icons-material/CircleOutlined";
 function RecentStatuses(props: { runs: Array<Run> | undefined }) {
   function statusChip(index: number) {
     if (props.runs === undefined) {
-      return <RunStateChip key={index}/>;
+      return <RunStateChip key={index} />;
     }
     if (props.runs.length > index) {
-      return <RunStateChip state={props.runs[index].future_state} key={index}/>
+      return (
+        <RunStateChip state={props.runs[index].future_state} key={index} />
+      );
     } else {
-      return <CircleOutlined color="disabled" key={index}/>
+      return <CircleOutlined color="disabled" key={index} />;
     }
   }
-  return (
-    <>
-      {[...Array(5)].map((e, i) => statusChip(i))}
-    </>
-    );
-};
-
+  return <>{[...Array(5)].map((e, i) => statusChip(i))}</>;
+}
 
 function PipelineRow(props: { run: Run }) {
   let run = props.run;
@@ -35,18 +32,19 @@ function PipelineRow(props: { run: Run }) {
   const [isLoaded, setIsLoaded] = useState(false);
   const [runs, setRuns] = useState<Array<Run> | undefined>(undefined);
 
-  
   useEffect(() => {
-    let filters = JSON.stringify({calculator_path: {eq: run.calculator_path}})
+    let filters = JSON.stringify({
+      calculator_path: { eq: run.calculator_path },
+    });
 
     fetch("/api/v1/runs?limit=5&filters=" + filters)
-      .then(res => res.json())
+      .then((res) => res.json())
       .then(
-        (result: RunListPayload) => { 
+        (result: RunListPayload) => {
           setRuns(result.content);
           setIsLoaded(true);
         },
-        (error) => { 
+        (error) => {
           setError(error);
           setIsLoaded(true);
         }
@@ -54,10 +52,10 @@ function PipelineRow(props: { run: Run }) {
   }, [run.calculator_path]);
 
   let startedAt = new Date(run.started_at || run.created_at);
-  let endedAt = new Date()
+  let endedAt = new Date();
   let endTimeString = run.failed_at || run.resolved_at;
   if (endTimeString) {
-    endedAt = new Date(endTimeString)
+    endedAt = new Date(endTimeString);
   }
 
   let durationMS: number = endedAt.getTime() - startedAt.getTime();
@@ -65,38 +63,39 @@ function PipelineRow(props: { run: Run }) {
   return (
     <TableRow key={run.id}>
       <TableCell key="name">
-      <Link href={"/runs/" + run.id} underline="hover">
-        <Typography variant="h6">
-          {run.name}
-            </Typography>
+        <Link href={"/runs/" + run.id} underline="hover">
+          <Typography variant="h6">{run.name}</Typography>
         </Link>
         <Typography fontSize="small" color="GrayText">
-          <code>
-            {run.calculator_path}
-          </code>
-          </Typography>
+          <code>{run.calculator_path}</code>
+        </Typography>
       </TableCell>
       <TableCell key="last-run">
-        <Typography fontSize='small' color='GrayText'>
+        <Typography fontSize="small" color="GrayText">
           {durationMS / 1000} seconds on&nbsp;
-          {(new Date(run.created_at)).toLocaleString()}
-          </Typography>
+          {new Date(run.created_at).toLocaleString()}
+        </Typography>
       </TableCell>
       <TableCell key="status">
         <RecentStatuses runs={runs} />
       </TableCell>
     </TableRow>
-  );  
-};
+  );
+}
 
-
-function Dashboard() {    
+function Dashboard() {
   return (
     <>
-      <Typography variant="h4" component="h2">Pipelines</Typography>
-      <RunList columns={['Name', 'Last run', 'Status']} groupBy="calculator_path" filters={{ parent_id: { eq: null } }}>
-        {(run: Run) => <PipelineRow run={run} key={run.id}/>}
-        </RunList>
+      <Typography variant="h4" component="h2">
+        Pipelines
+      </Typography>
+      <RunList
+        columns={["Name", "Last run", "Status"]}
+        groupBy="calculator_path"
+        filters={{ parent_id: { eq: null } }}
+      >
+        {(run: Run) => <PipelineRow run={run} key={run.id} />}
+      </RunList>
     </>
   );
 }
--- glow/ui/src/Payloads.tsx
+++ glow/ui/src/Payloads.tsx
@@ -1,10 +1,10 @@
-import { Run } from './Models';
+import { Run } from "./Models";
 
 export type RunListPayload = {
-    current_page_url: string,
-    next_page_url: string | undefined,
-    limit: number,
-    next_cursor: string | undefined,
-    after_cursor_count: number,
-    content: Array<Run>,
-}
+  current_page_url: string;
+  next_page_url: string | undefined;
+  limit: number;
+  next_cursor: string | undefined;
+  after_cursor_count: number;
+  content: Array<Run>;
+};
