6c3d78692bfecc1e9c4713dca3b0786e819133a1
Emmanuel Turlay <emmanuel@sematic.ai>
2022-05-30 12:37:35 -0700

    graph algo tests

--- glow/db/models/base.py
+++ glow/db/models/base.py
@@ -2,4 +2,15 @@
 from sqlalchemy.ext.declarative import declarative_base
 
 
-Base = declarative_base()
+class AbstractBase:
+    def __repr__(self):
+        return "{}({})".format(
+            self.__class__.__name__,
+            ", ".join(
+                "{}={}".format(column.name, repr(getattr(self, column.name)))
+                for column in self.__class__.__table__.columns
+            ),
+        )
+
+
+Base = declarative_base(cls=AbstractBase)
