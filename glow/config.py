38aa9b793a541d65af7efde435891ba733d60ab0
Emmanuel Turlay <emmanuel@sematic.ai>
2022-06-03 18:01:32 -0700

    rename ci req file

--- glow/config.py
+++ glow/config.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from enum import Enum
 import os
 import pathlib
+from urllib.parse import urljoin
 
 
 _DEFAULT_CONFIG_DIR = ".glow"
@@ -21,12 +22,16 @@ def _get_config_dir() -> str:
 
 @dataclass
 class Config:
-    api_url: str
+    api_server_url: str
     api_version: int
     config_dir: str = _get_config_dir()
 
+    @property
+    def api_url(self):
+        return urljoin(self.api_server_url, "api/v{}".format(self.api_version))
 
-_LOCAL_CONFIG = Config(api_url="http://127.0.0.1:5000/", api_version=1)
+
+_LOCAL_CONFIG = Config(api_server_url="http://127.0.0.1:5000", api_version=1)
 
 
 class StandardConfigs(Enum):
