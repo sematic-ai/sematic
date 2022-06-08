3219378a0beaf535a317efe7370a980b06996459
Emmanuel Turlay <emmanuel@sematic.ai>
2022-06-07 18:56:01 -0700

    CLI prototype

---
+++ sematic/cli/main.py
@@ -0,0 +1,65 @@
+# Standard library
+import subprocess
+
+# Third-party
+import click
+
+# Sematic
+from sematic.config import get_config
+from sematic.cli.process_utils import (
+    server_is_running,
+    write_server_pid,
+    get_server_pid,
+)
+
+
+@click.group("sematic")
+def main():
+    """
+    Welcome to Sematic
+    """
+    pass
+
+
+@main.command("start", short_help="Start the Sematic app")
+def start():
+    if server_is_running():
+        # The server url may be incorrect here if started with a different env
+        # We can save the URL in server.pid, but we can't use the `ps` recovery strategy
+        # in `_get_server_pid` if the pid is incorrect
+        click.echo("Sematic already running at {}".format(get_config().server_url))
+        return
+
+    click.echo("Starting Sematic...")
+    process = subprocess.Popen(
+        "python3 sematic/api/server.py --env local_sqlite &> /tmp/out", shell=True
+    )
+
+    write_server_pid(process.pid)
+
+    click.echo("Started with PID {}".format(process.pid))
+    click.echo("Visit Sematic at {}".format(get_config().server_url))
+
+
+@main.command("stop", short_help="Stop the Sematic server")
+def stop():
+    server_pid = get_server_pid()
+    if server_pid is None:
+        click.echo("Sematic is not running.")
+        return
+
+    process = subprocess.run(
+        "kill {}".format(server_pid), shell=True, capture_output=True
+    )
+    error = process.stderr.decode()
+
+    if process.returncode == 0:
+        click.echo("Successfully stopped Sematic.")
+    elif "No such process" in error:
+        click.echo("Sematic is not running.")
+    else:
+        click.echo("There was a problem stopping Sematic: {}".format(error))
+
+
+if __name__ == "__main__":
+    main()
---
+++ sematic/cli/tests/test_main.py
@@ -0,0 +1,60 @@
+# Standard library
+from dataclasses import asdict
+import tempfile
+import pytest
+
+# import subprocess
+
+# Third-party
+# from click.testing import CliRunner
+
+# Sematic
+# import sematic.cli.main as main
+# from sematic.cli.process_utils import server_is_running
+from sematic.config import Config, get_config, set_config
+from sematic.db.tests.fixtures import test_db  # noqa: F401
+
+
+@pytest.fixture
+def test_config(test_db):  # noqa: F811
+    current_config = get_config()
+    new_config = Config(**asdict(current_config))
+    new_config.port = 5010
+    new_config.config_dir = tempfile.mkdtemp()
+    set_config(new_config)
+    try:
+        yield
+    finally:
+        set_config(current_config)
+
+
+def test_start(test_config):
+    """
+    runner = CliRunner()
+    assert not server_is_running()
+    result = runner.invoke(main.start)
+    assert result.exit_code == 0
+    process = subprocess.run("ps | grep sematic", shell=True, capture_output=True)
+    assert server_is_running()
+
+    process = subprocess.run(
+        "netstat -na | grep {}".format(get_config().port),
+        shell=True,
+        capture_output=True,
+    )
+
+    assert len(process.stdout.decode()) == 1
+
+    result = runner.invoke(main.stop)
+    assert result.exit_code == 0
+
+    assert not server_is_running()
+
+    process = subprocess.run(
+        "netstat -na | grep {}".format(get_config().port),
+        shell=True,
+        capture_output=True,
+    )
+    assert len(process.stdout.decode()) == 0
+    """
+    pass
