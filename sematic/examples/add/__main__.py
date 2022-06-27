860c92d86856f6f4d803d9ae7defeb262c4b4197
Emmanuel Turlay <emmanuel@sematic.ai>
2022-06-26 18:07:01 -0700

    better docstrings

--- sematic/examples/add/__main__.py
+++ sematic/examples/add/__main__.py
@@ -3,11 +3,7 @@ import logging
 
 # Sematic
 from sematic import LocalResolver
-from sematic.examples.add.calculators import (  # noqa:F401
-    pipeline,
-    sum_list,
-    using_dataclass,
-)
+from sematic.examples.add.pipeline import pipeline
 
 
 if __name__ == "__main__":
@@ -16,10 +12,6 @@ if __name__ == "__main__":
     future = pipeline(1, 2, 3).set(
         name="Basic add example pipeline", tags=["example", "basic", "final"]
     )
-
-    # future = sum_list([1, 2, 3, 4, 5], 0.5)
-
-    # future = using_dataclass(dict(foo=[1, 2, 3], bar=dict(barr=[3, 4, 5])))
     result = future.resolve(LocalResolver())
 
     logging.info(result)
