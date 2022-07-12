7649a95750f6aa24f9102846d1cc53a0bd58c2d6
Emmanuel Turlay <emmanuel@sematic.ai>
2022-07-12 16:22:20 -0500

    Fixes Timestamp JSON serialization by defaulting to `str` (#64)
    Fixing summary of timestamp

--- sematic/types/types/pandas/tests/test_dataframe.py
+++ sematic/types/types/pandas/tests/test_dataframe.py
@@ -1,9 +1,12 @@
 # Third-party
+import datetime
 import os
 import pandas
+import json
 
 # Sematic
 from sematic.types.serialization import get_json_encodable_summary
+from sematic.db.models.factories import make_artifact
 
 
 def test_dataframe_summary():
@@ -18,3 +21,14 @@ def test_dataframe_summary():
     assert len(list(summary["dataframe"].values())[0]) == 5
     assert len(summary["describe"]) == 12
     assert len(summary["isna"]) == 19
+
+
+def test_dataframe_datetime():
+    timestamp = datetime.datetime.now()
+    df = pandas.DataFrame([dict(a=timestamp)])
+
+    artifact = make_artifact(df, pandas.DataFrame)
+
+    assert json.loads(artifact.json_summary)["dataframe"] == {
+        "a": {"0": str(timestamp)}
+    }
