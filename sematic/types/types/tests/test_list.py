6998ba97c5f9d32527b21ec07842483c9a048f94
Emmanuel Turlay <emmanuel@sematic.ai>
2022-07-19 18:12:19 +0000

    Extending test converage

--- sematic/types/types/tests/test_list.py
+++ sematic/types/types/tests/test_list.py
@@ -174,8 +174,12 @@ def test_to_binary_arbitrary():
     json_encodable = value_to_json_encodable([A(), A()], type_)
 
     assert json_encodable == [
-        "gAWVMAAAAAAAAACMI3NlbWF0aWMudHlwZXMudHlwZXMudGVzdHMudGVzdF9saXN0lIwBQZSTlCmBlC4=",  # noqa: E501
-        "gAWVMAAAAAAAAACMI3NlbWF0aWMudHlwZXMudHlwZXMudGVzdHMudGVzdF9saXN0lIwBQZSTlCmBlC4=",  # noqa: E501
+        {
+            "pickle": "gAWVMAAAAAAAAACMI3NlbWF0aWMudHlwZXMudHlwZXMudGVzdHMudGVzdF9saXN0lIwBQZSTlCmBlC4="  # noqa: E501
+        },
+        {
+            "pickle": "gAWVMAAAAAAAAACMI3NlbWF0aWMudHlwZXMudHlwZXMudGVzdHMudGVzdF9saXN0lIwBQZSTlCmBlC4="  # noqa: E501
+        },
     ]
 
 
