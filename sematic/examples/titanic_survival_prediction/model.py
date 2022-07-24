1607a73f6d10f62ab880f0b5d983f807ff3c78c7
Emmanuel Turlay <emmanuel@sematic.ai>
2022-07-24 11:45:19 -0700

    Fix linting of titanic example (#69)
    Fix linting

--- sematic/examples/titanic_survival_prediction/model.py
+++ sematic/examples/titanic_survival_prediction/model.py
@@ -7,10 +7,11 @@ import sematic
 # Titianic survival prediction example
 import sematic.examples.titanic_survival_prediction.consts as consts
 
+
 @sematic.func
 def init_model() -> DecisionTreeClassifier:
     return DecisionTreeClassifier(
-        random_state= consts.RAND_STATE,
-        class_weight= consts.DECISION_TREE_CLASS_WEIGHT,
-        max_depth= consts.DECISION_TREE_MAX_DEPTH
+        random_state=consts.RAND_STATE,
+        class_weight=consts.DECISION_TREE_CLASS_WEIGHT,
+        max_depth=consts.DECISION_TREE_MAX_DEPTH,
     )
