2d56759b73036fb897b1e315e535ae673e584dad
Emmanuel Turlay <emmanuel@sematic.ai>
2022-07-24 11:33:20 -0700

    Fix linting

--- sematic/examples/titanic_survival_prediction/test.py
+++ sematic/examples/titanic_survival_prediction/test.py
@@ -8,8 +8,11 @@ import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import confusion_matrix, classification_report
 
+
 @sematic.func
-def eval_model(model: DecisionTreeClassifier, X_test: pd.DataFrame, y_test: pd.DataFrame) -> EvaluationOutput:
+def eval_model(
+    model: DecisionTreeClassifier, X_test: pd.DataFrame, y_test: pd.DataFrame
+) -> EvaluationOutput:
     y_pred = model.predict(X_test)
     cr = pd.DataFrame(classification_report(y_test, y_pred, output_dict=True)).T
     cm = pd.DataFrame(confusion_matrix(y_test, y_pred))
--- sematic/examples/titanic_survival_prediction/train.py
+++ sematic/examples/titanic_survival_prediction/train.py
@@ -5,7 +5,10 @@ import sematic
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 
+
 @sematic.func
-def train_model(model: DecisionTreeClassifier, X_train: pd.DataFrame, y_train: pd.DataFrame) -> DecisionTreeClassifier:
+def train_model(
+    model: DecisionTreeClassifier, X_train: pd.DataFrame, y_train: pd.DataFrame
+) -> DecisionTreeClassifier:
     model.fit(X_train, y_train)
     return model
