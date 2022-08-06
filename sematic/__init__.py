a3e619eb6daed400a30c2992b7f00261bad1aef0
augray <augray@users.noreply.github.com>
2022-08-05 19:42:51 -0700

    Add friendly error message for bad python versions (#78)
    * Add friendly error message for bad python versions
    
    * Update changelog
    
    Co-authored-by: Joshua Bauer <josh@Joshuas-MacBook-Pro.local>

--- sematic/__init__.py
+++ sematic/__init__.py
@@ -1,13 +1,27 @@
 """
 Sematic Public API
 """
-from sematic.calculator import func  # noqa: F401
-from sematic.resolver import Resolver  # noqa: F401
-from sematic.resolvers.local_resolver import LocalResolver  # noqa: F401
-from sematic.resolvers.cloud_resolver import CloudResolver  # noqa: F401
-from sematic.resolvers.resource_requirements import (  # noqa: F401
+import sys
+
+MIN_PYTHON_VERSION = (3, 9, 0)
+_CURRENT_PYTHON_VERSION = sys.version_info[0:3]
+
+if _CURRENT_PYTHON_VERSION < MIN_PYTHON_VERSION:
+    _min_version_as_str = ".".join(str(i) for i in MIN_PYTHON_VERSION)
+    _current_version_as_str = ".".join(str(i) for i in _CURRENT_PYTHON_VERSION)
+    raise RuntimeError(
+        f"Sematic requires python to be at {_min_version_as_str} or above, "
+        f"but you are running {_current_version_as_str}. Please upgrade "
+        f"to continue."
+    )
+
+from sematic.calculator import func  # noqa: F401,E402
+from sematic.resolver import Resolver  # noqa: F401,E402
+from sematic.resolvers.local_resolver import LocalResolver  # noqa: F401,E402
+from sematic.resolvers.cloud_resolver import CloudResolver  # noqa: F401,E402
+from sematic.resolvers.resource_requirements import (  # noqa: F401,E402
     ResourceRequirements,
     KubernetesResourceRequirements,
 )
-import sematic.types  # noqa: F401
-import sematic.future_operators  # noqa: F401
+import sematic.types  # noqa: F401,E402
+import sematic.future_operators  # noqa: F401,E402
