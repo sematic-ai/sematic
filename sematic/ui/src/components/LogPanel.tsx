acd45b98582be08001d45e61b41036344568db86
augray <apps@joshbauer.email>
2022-09-28 16:29:06 -0700

    Working Scrolling Log View

--- sematic/ui/src/components/LogPanel.tsx
+++ sematic/ui/src/components/LogPanel.tsx
@@ -1,19 +1,18 @@
 import GrafanaPanel from "../addons/grafana/GrafanaPanel";
 import Exception from "./Exception";
-import { Box, Button } from "@mui/material";
+import { Box } from "@mui/material";
 import { Run } from "../Models";
 import { fetchJSON } from "../utils";
-import { useCallback, useContext, useEffect, useMemo, useState } from "react";
+import { useCallback, useContext, useState } from "react";
 import { UserContext } from "..";
 import { LogLineRequestResponse } from "../Payloads";
 import ScrollingLogView from "./ScrollingLogView";
-import { GetLines, MoreLinesCallback } from "./ScrollingLogView";
+import { MoreLinesCallback } from "./ScrollingLogView";
 
 
 export default function LogPanel(props: { run: Run }) {
   const { run } = props;
   const { user } = useContext(UserContext);
-  const [isLoaded, setIsLoaded] = useState(false);
   const [error, setError] = useState<Error | undefined>(undefined);
 
   const loadLogs = useCallback((cursor: string | null, callback: MoreLinesCallback) => {
@@ -25,19 +24,25 @@ export default function LogPanel(props: { run: Run }) {
       url: url,
       apiKey: user?.api_key,
       callback: (payload: LogLineRequestResponse) => {
-        setIsLoaded(true);
         callback(payload.content.lines, payload.content.continuation_cursor, payload.content.log_unavailable_reason);
       },
       setError: setError,
     });
-  }, [run]);
+  }, [run, user?.api_key]);
+
+  const standardLogView = (
+    <ScrollingLogView getLines={loadLogs} initialLines={[]} initialCursor={null} logSource={run.id}/>
+  );
+  const logErrorView = (
+    <div>The server returned an error when asked for logs for this run.</div>
+  );
+  const logView = error === undefined ? standardLogView : logErrorView ;
 
   return (
     <Box>
         {run.exception && <Exception exception={run.exception} />}
         <GrafanaPanel run={run} />
-        
-        <ScrollingLogView getLines={loadLogs} initialLines={[]} initialCursor={null}/>
+        {logView}
     </Box>
   );
 }
--- sematic/ui/src/components/ScrollingLogView.tsx
+++ sematic/ui/src/components/ScrollingLogView.tsx
@@ -1,5 +1,5 @@
-import { useCallback, useContext, useEffect, useMemo, useState } from "react";
-import { Box, Button } from "@mui/material";
+import { useEffect, useState, useCallback } from "react";
+import { Box } from "@mui/material";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 export type MoreLinesCallback = (lines: string[], cursor: string | null, noLinesReason: string | null) => void;
@@ -7,49 +7,45 @@ export type GetLines = (cursor: string | null, moreLinesCallback: MoreLinesCallb
 
 const DEFAULT_NO_LINES_REASON = "No more matching lines";
 
-export default function ScrollingLogView(props: { getLines: GetLines, initialLines: string[], initialCursor: string | null}) {  
-    const { getLines, initialLines, initialCursor } = props;
+export default function ScrollingLogView(props: { getLines: GetLines, initialLines: string[], initialCursor: string | null, logSource: string}) {  
+    const { getLines, initialLines, initialCursor, logSource } = props;
 
     const [existingLines, setLines] = useState<string[]>(initialLines);
     const [currentCursor, setCursor] = useState<string | null>(initialCursor);
     const [hasMore, setHasMore] = useState(true);
     const [currentNoLinesReason, setNoLinesReason] = useState<string | null>(DEFAULT_NO_LINES_REASON);
 
-    const handleLogLines = (lines: string[], cursor: string | null, noLinesReason: string | null) => {
+    const handleLogLines = useCallback((lines: string[], cursor: string | null, noLinesReason: string | null) => {
         var newLines: string[] = existingLines.concat(lines);
         setLines(newLines);
         setCursor(cursor);
         setHasMore(cursor != null);
-        if (cursor == null) {
-            // there's no more to display
-            setNoLinesReason(DEFAULT_NO_LINES_REASON);
-        } else {
-            setNoLinesReason(noLinesReason);
-        }
-    };
+        setNoLinesReason(noLinesReason === "" || noLinesReason === null? DEFAULT_NO_LINES_REASON : noLinesReason);
+    }, [existingLines]);
 
-    const next = () => {
+    const next = useCallback(() => {
         getLines(currentCursor, handleLogLines);
-    };
+    }, [getLines, currentCursor, handleLogLines]);
 
-    useMemo(() => {
-        setLines([]);
+    useEffect(() => {
         setCursor(null);
+        setLines([]);
         setHasMore(true);
         next();
-    }, [getLines]);
+    }, [logSource]);
 
     const noMoreLinesIndicator = (
         <div className="no-more-indicator">------ {currentNoLinesReason} ------</div>
     );
+    const scrollerId = "scrolling-logs-" + logSource;
 
     return (
       <Box>
-          <div id="scrolling-logs" className="ScrollingLogView">
+          <div id={scrollerId} className="ScrollingLogView">
             <InfiniteScroll
                 dataLength={existingLines.length}
                 next={next}
-                scrollableTarget="scrolling-logs"
+                scrollableTarget={scrollerId}
                 hasMore={hasMore}
                 loader={<h4>Loading...</h4>}
                 endMessage={noMoreLinesIndicator}
