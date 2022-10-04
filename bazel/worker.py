8c141598a481cdc1ec592dc118fcdf7556713746
augray <augray@users.noreply.github.com>
2022-10-04 14:21:10 -0700

    Wrap worker.py main more consistently (#178)
    When people use the sematic_pipeline bazel macro and don't use dev=True, it points them to the worker.py in bazel/worker.py rather than sematic/resolvers/worker.py. It imported the main from there, but it missed the code that wrapped main to set up logging properly. This PR makes it so both worker.py scripts use the same log wrapping for main.

--- bazel/worker.py
+++ bazel/worker.py
@@ -5,7 +5,7 @@ We need to do this because we don't want to use the current worker at
 //sematic/resolvers:worker we want to use the one in the wheel that the user has
 installed.
 """
-from sematic.resolvers.worker import main, parse_args
+from sematic.resolvers.worker import wrap_main_with_logging
 
 if __name__ == "__main__":
-    main(**vars(parse_args()))
+    wrap_main_with_logging()
