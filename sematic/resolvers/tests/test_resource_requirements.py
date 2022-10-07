6c33ca71976490805c8e2d67126f47e1396d53c9
augray <augray@users.noreply.github.com>
2022-10-07 16:58:10 -0700

    Add support for specifying k8s tolerations (#192)
    People may want/need more control over which nodes their jobs run on. K8s tolerations is a mechanism many people use to achieve this. If we don't support them, people may not be able to run their Sematic funcs where they want them to go. This adds support for it.
    
    It also adds support for enums, because the natural way to support tolerations includes some enum types that are currently serialized/deserialized with Sematic's type stuff. It fails without actual enum support. I've wanted enum support a few times anyway, so I took this opportunity to "bite the bullet."

---
+++ sematic/resolvers/tests/test_resource_requirements.py
@@ -0,0 +1,55 @@
+import pytest
+
+# Sematic
+from sematic.resolvers.resource_requirements import (
+    KubernetesResourceRequirements,
+    KubernetesSecretMount,
+    KubernetesToleration,
+    KubernetesTolerationEffect,
+    KubernetesTolerationOperator,
+    ResourceRequirements,
+)
+from sematic.types.serialization import (
+    value_from_json_encodable,
+    value_to_json_encodable,
+)
+
+
+def test_is_serializable():
+    requirements = ResourceRequirements(
+        kubernetes=KubernetesResourceRequirements(
+            node_selector={"foo": "bar"},
+            requests={"cpu": "500m", "memory": "100Gi"},
+            secret_mounts=KubernetesSecretMount(
+                environment_secrets={"a": "b"},
+                file_secret_root_path="/foo/bar",
+                file_secrets={"c": "d"},
+            ),
+            tolerations=[
+                KubernetesToleration(
+                    key="k",
+                    value="v",
+                    effect=KubernetesTolerationEffect.NoExecute,
+                    operator=KubernetesTolerationOperator.Equal,
+                    toleration_seconds=42,
+                )
+            ],
+        )
+    )
+    encoded = value_to_json_encodable(requirements, ResourceRequirements)
+    decoded = value_from_json_encodable(encoded, ResourceRequirements)
+    assert decoded == requirements
+
+
+def test_validation():
+    with pytest.raises(
+        ValueError,
+        match="toleration_seconds should only be specified when the effect is NoExecute.",
+    ):
+        KubernetesToleration(
+            key="k",
+            value="v",
+            effect=KubernetesTolerationEffect.PreferNoSchedule,
+            operator=KubernetesTolerationOperator.Equal,
+            toleration_seconds=42,
+        )
---
+++ sematic/types/types/tests/test_enum.py
@@ -0,0 +1,67 @@
+# Standard Library
+from enum import Enum, unique
+
+import pytest
+
+# Sematic
+from sematic.types.serialization import (
+    value_from_json_encodable,
+    value_to_json_encodable,
+)
+from sematic.types.types.enum import (
+    _enum_from_encodable,
+    _enum_to_encodable,
+    can_cast_type,
+)
+
+
+class SomethingExotic:
+    def __init__(self, number):
+        self.number = number
+
+
+@unique
+class Color(Enum):
+    RED = "RED"
+    GREEN = "GREEN"
+    BLUE = "BLUE"
+
+
+@unique
+class ExoticNumbers(Enum):
+    MEANING_OF_LIFE = SomethingExotic(42)
+    LUCKY_NUMBER = SomethingExotic(7)
+
+
+def test_to_from_encodable():
+    encoded = _enum_to_encodable(Color.RED, Color)
+    assert encoded == "RED"
+
+    # ensures registration worked
+    assert encoded == value_to_json_encodable(Color.RED, Color)
+    decoded = _enum_from_encodable(encoded, Color)
+    assert decoded == value_from_json_encodable(encoded, Color)
+    assert decoded == Color.RED
+
+    encoded = _enum_to_encodable(ExoticNumbers.LUCKY_NUMBER, ExoticNumbers)
+    assert encoded == "LUCKY_NUMBER"
+    decoded = _enum_from_encodable(encoded, ExoticNumbers)
+    assert decoded == ExoticNumbers.LUCKY_NUMBER
+    assert decoded.value.number == 7
+
+    with pytest.raises(
+        ValueError, match=r"The value 'ExoticNumbers.LUCKY_NUMBER' is not a Color"
+    ):
+        _enum_to_encodable(ExoticNumbers.LUCKY_NUMBER, Color)
+
+    with pytest.raises(ValueError, match=r"The type Color has no value 'LUCKY_NUMBER'"):
+        _enum_from_encodable("LUCKY_NUMBER", Color)
+
+
+def test_can_cast_type():
+    assert can_cast_type(Color, Color) == (True, None)
+    assert can_cast_type(Color, SomethingExotic) == (
+        False,
+        "<enum 'Color'> does not match "
+        "<class 'sematic.types.types.tests.test_enum.SomethingExotic'>",
+    )
