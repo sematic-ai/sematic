8ce01b2bfdf49a73f28f2a9e9d921f4002a7e42f
augray <apps@joshbauer.email>
2022-10-07 11:05:56 -0700

    tests

--- sematic/types/types/enum.py
+++ sematic/types/types/enum.py
@@ -3,7 +3,12 @@ import typing
 from enum import Enum
 
 # Sematic
-from sematic.types.registry import register_can_cast, register_to_json_encodable_summary
+from sematic.types.registry import (
+    register_can_cast,
+    register_from_json_encodable,
+    register_to_json_encodable,
+    register_to_json_encodable_summary,
+)
 
 
 @register_can_cast(Enum)
@@ -13,12 +18,12 @@ def can_cast_type(
     """
     Type casting logic for `Enum`.
 
-    The to_type must be a parent type of the from_type for castability
+    The types must be equal
     """
-    if issubclass(from_type, to_type):
+    if issubclass(from_type, to_type) and issubclass(to_type, from_type):
         return True, None
 
-    return False, "{} is not a child class of {}".format(from_type, to_type)
+    return False, "{} does not match {}".format(from_type, to_type)
 
 
 # Default safe_cast behavior is sufficient
@@ -26,4 +31,21 @@ def can_cast_type(
 
 @register_to_json_encodable_summary(Enum)
 def _enum_summary(value: Enum, _) -> str:
+    return _enum_to_encodable(value, _)
+
+
+@register_to_json_encodable(Enum)
+def _enum_to_encodable(value: Enum, type_: typing.Type[Enum]) -> str:
+    # Ex: foo.bar.Color.RED
+    if not isinstance(value, type_):
+        raise ValueError(f"The value '{value}' is not a {type_.__name__}")
     return value.name
+
+
+@register_from_json_encodable(Enum)
+def _enum_from_encodable(value: str, type_: typing.Type[Enum]):
+    value_name = value.split(".")[-1]
+    if not hasattr(type_, value_name):
+        raise ValueError(f"The type {type_.__name__} has no value '{value_name}'")
+    deserialized = type_[value_name]
+    return deserialized
