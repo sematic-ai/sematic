490a66112ea23362bda47ffc7d9dcee047d2c1ba
tscurtu <tudor@sematic.dev>
2022-10-14 10:45:30 -0700

    Add Sematic version cli command (#214)

--- sematic/cli/main.py
+++ sematic/cli/main.py
@@ -5,6 +5,7 @@ import sematic.cli.run  # noqa: F401
 import sematic.cli.settings  # noqa: F401
 import sematic.cli.start  # noqa: F401
 import sematic.cli.stop  # noqa: F401
+import sematic.cli.version  # noqa: F401
 from sematic.cli.cli import cli
 
 if __name__ == "__main__":
