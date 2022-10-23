7585c39ac9c1b46c8680185f08aea4d6cc27a8b1
Emmanuel Turlay <emmanuel@sematic.ai>
2022-10-23 01:34:19 +0000

    Fix build

--- sematic/resolvers/tests/test_cloud_resolver.py
+++ sematic/resolvers/tests/test_cloud_resolver.py
@@ -99,7 +99,7 @@ def test_simulate_cloud_exec(
     mock_update_run_future_states.side_effect = fake_update_run_future_states
 
     mock_schedule_resolution.assert_called_once_with(
-        resolution_id=future.id, max_parallelism=None
+        resolution_id=future.id, max_parallelism=None, rerun_from=None
     )
     assert api_client.get_resolution(future.id).status == ResolutionStatus.CREATED.value
 
