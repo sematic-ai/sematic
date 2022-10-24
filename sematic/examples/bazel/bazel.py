5b5e426e1085078cafe0ce68ecc2afeab5f93a7c
Josh Bauer <josh@sematic.dev>
2022-10-24 16:42:43 +0000

    reproduce

--- sematic/examples/bazel/bazel.py
+++ sematic/examples/bazel/bazel.py
@@ -15,7 +15,8 @@ if __name__ == "__main__":
     parser.add_argument("--detach", default=False, action="store_true")
 
     args = parser.parse_args()
+    max_parallel = 2
 
     logger.info("Starting bazel example...")
-    future = pipeline(1, 2, 3)
-    future.resolve(sematic.CloudResolver(detach=args.detach))
+    future = pipeline(1, 2, 3).set(tags=[f"max_parallel:{max_parallel}"])
+    future.resolve(sematic.CloudResolver(detach=args.detach, max_parallelism=max_parallel))
--- sematic/examples/bazel/pipeline.py
+++ sematic/examples/bazel/pipeline.py
@@ -1,17 +1,44 @@
 # Standard Library
 import time
 
+from enum import Enum, unique
+
 # Sematic
 import sematic
 
+@unique
+class Color(Enum):
+    RED = "RED"
+    GREEN = "GREEN"
+    BLUE = "BLUE"
 
-@sematic.func(inline=False)
-def add(a: float, b: float) -> float:
+
+@sematic.func(
+    inline=False,
+    resource_requirements=sematic.ResourceRequirements(
+        kubernetes=sematic.KubernetesResourceRequirements(
+            tolerations=[
+                sematic.KubernetesToleration(
+                    key="foo",
+                    operator=sematic.KubernetesTolerationOperator.Exists,
+                    effect=sematic.KubernetesTolerationEffect.NoSchedule,
+                    #toleration_seconds=42,
+                )
+            ],
+        ),
+    )
+)
+def add(a: float, b: float, color: Color=Color.RED) -> float:
     time.sleep(5)
     return a + b
 
+class SomeException(Exception):
+    pass
 
-@sematic.func(inline=False)
+import random
+import logging
+
+@sematic.func(base_image_tag="cuda", inline=False, retry=sematic.RetrySettings(exceptions=(SomeException,), retries=15))
 def add3(a: float, b: float, c: float) -> float:
     return add(add(a, b), c)
 
@@ -28,6 +55,34 @@ def fail_nested() -> float:
 
 @sematic.func(inline=True)
 def pipeline(a: float, b: float, c: float) -> float:
-    # return add(add3(a, b, c), add(a, b))
+    #return add(add3(a, b, c), add(a, b))
     # return add(a, b)
-    return fail()
+    #return fail()
+    sleep_time = 600
+    return int_sum([sleep_for(sleep_time) for _ in range(10)])
+
+from typing import List
+
+@sematic.func(inline=False)
+def do_something() -> str:
+    return "42"
+
+@sematic.func(inline=False)
+def sleep_for(n_seconds: int) -> int:
+    started = time.time()
+    while time.time() < started + n_seconds:
+        print(f"Sleeping at {time.time()}...")
+        time.sleep(1)
+    return n_seconds
+
+
+@sematic.func(inline=False)
+def int_sum(elements: List[int]) -> float:
+    return 1.0 * sum(elements)
+
+#@sematic.func(inline=False)
+#def pipeline(a: float, b: float, c: float) -> List[str]:
+#    result = [do_something() for _ in range(0, 2)]
+#    return result
+
+#print(pipeline().resolve())
