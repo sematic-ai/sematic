3ac82d75023f5a1e2df43291870418a8a37205dc
augray <augray@users.noreply.github.com>
2022-10-24 21:22:44 -0700

    Enable mocking Sematic funcs (#252)

---
+++ sematic/testing/tests/test_mock_funcs.py
@@ -0,0 +1,61 @@
+# Standard Library
+from typing import List
+
+import pytest
+
+# Sematic
+from sematic.calculator import func
+from sematic.resolvers.silent_resolver import SilentResolver
+from sematic.testing.mock_funcs import mock_sematic_funcs
+
+
+@func
+def pipeline() -> int:
+    return do_sum([remote_only_func(1), remote_only_func(2), identity_func(3)])
+
+
+@func
+def do_sum(ints: List[int]) -> int:
+    return sum(ints)
+
+
+@func
+def remote_only_func(x: int) -> int:
+    raise ValueError("Oh no! This function doesn't work when you're testing")
+
+
+@func
+def identity_func(x: int) -> int:
+    return x
+
+
+def test_mock_sematic_funcs():
+    with pytest.raises(ValueError, match=r"Oh no.*"):
+        pipeline().resolve(SilentResolver())
+
+    with mock_sematic_funcs([remote_only_func]) as mock_funcs:
+        mock_funcs[remote_only_func].mock.return_value = 1
+        result = pipeline().resolve(SilentResolver())
+        assert result == 5
+
+
+def test_mock_sematic_funcs_use_original():
+    with mock_sematic_funcs([remote_only_func, identity_func]) as mock_funcs:
+        mock_funcs[remote_only_func].mock.return_value = 1
+        mock_funcs[identity_func].mock.side_effect = mock_funcs[identity_func].original
+        result = pipeline().resolve(SilentResolver())
+        assert result == 5
+
+        mock_funcs[identity_func].mock.assert_called()
+
+    assert identity_func(16).resolve(SilentResolver()) == 16
+
+
+def test_mock_sematic_funcs_still_type_checks():
+    with pytest.raises(
+        TypeError,
+        match=r"for 'sematic.testing.tests.test_mock_funcs.remote_only_func'.*",
+    ):
+        with mock_sematic_funcs([remote_only_func]) as mock_funcs:
+            mock_funcs[remote_only_func].mock.return_value = "this is the wrong type!"
+            pipeline().resolve(SilentResolver())
