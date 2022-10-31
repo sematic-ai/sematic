5097184971d0f029a6d5e6381079576038171a4c
tscurtu <tudor@sematic.dev>
2022-10-30 20:27:49 -0700

    Expose Kubernetes infra failures in the Run panel (#269)

--- sematic/retry_settings.py
+++ sematic/retry_settings.py
@@ -1,9 +1,12 @@
 # Standard Library
+import logging
 from dataclasses import dataclass, field
-from typing import Tuple, Type
+from typing import Optional, Tuple, Type
 
 # Sematic
 from sematic.utils.exceptions import ExceptionMetadata
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -28,11 +31,29 @@ class RetrySettings:
         if not isinstance(self.exceptions, (tuple, list, set)):
             raise ValueError(f"exceptions should be a tuple, got {self.exceptions}")
 
-    def should_retry(self, exception_metadata: ExceptionMetadata) -> bool:
+    def should_retry(
+        self, *exception_metadata_tuple: Optional[ExceptionMetadata]
+    ) -> bool:
         """
-        Should the given exception trigger a retry?
+        Should the given exceptions trigger a retry?
+
+        If this is called with no effective parameters or only with None values, it means
+        we are in an abnormal situation in which a future failed with no known cause.
+
+        Parameters
+        ----------
+        exception_metadata_tuple: Tuple[Optional[ExceptionMetadata]]
+            Metadata describing a series of exceptions that have affected execution.
         """
-        if not self._matches_exceptions(exception_metadata):
+        filtered_list = [e for e in exception_metadata_tuple if e is not None]
+        if len(filtered_list) == 0:
+            logger.warning(
+                "`should_retry` called with no effective exception metadata! "
+                "Unknown failure situation!"
+            )
+            return False
+
+        if not any(map(self._matches_exceptions, filtered_list)):
             return False
 
         return self.retry_count < self.retries
