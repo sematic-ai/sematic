932c1ffc6c298feefc75b136b6865798b736fced
Tudor Scurtu <tudor@sematic.dev>
2022-10-31 14:47:41 -0700

    Add testing pipeline

---
+++ sematic/examples/testing_pipeline/__main__.py
@@ -0,0 +1,187 @@
+"""
+Entry point for the testing pipeline.
+"""
+# Standard Library
+import argparse
+import logging
+import sys
+from typing import Dict
+
+# Sematic
+from sematic import CloudResolver, LocalResolver
+from sematic.examples.testing_pipeline.pipeline import testing_pipeline
+from sematic.resolvers.state_machine_resolver import StateMachineResolver
+
+logger = logging.getLogger(__name__)
+
+BAZEL_COMMAND = "bazel run //sematic/examples/testing_pipeline:__main__ --"
+DESCRIPTION = (
+    "This is the Sematic Testing Pipeline. "
+    "It is used to test the behavior of the Server and of the Resolver. "
+    "The arguments control the shape of the pipeline, as described in the individual "
+    "help strings. "
+    "Some of the arguments have dependencies on other arguments, and missing values will "
+    "be reported to the user. "
+    "At the end of the pipeline execution, the individual future outputs are collected "
+    "in a future list and reduced."
+)
+LOG_LEVEL_HELP = "The log level for the pipeline and Resolver. Defaults to INFO."
+CLOUD_HELP = (
+    "Whether to run the resolution in the cloud, or locally. Defaults to False."
+)
+DETACH_HELP = (
+    "When in `cloud` mode, whether to detach the execution of the driver job and have it "
+    "run on the remote cluster. This is the so called `fire-and-forget` mode. The shell "
+    "prompt will return as soon as the driver job as been submitted. Defaults to False."
+)
+RERUN_FROM_HELP = (
+    "The id of a run to rerun as part of a new pipeline resolution. This will copy the "
+    "previous resolution, while invalidating any failed runs, this specified run, and "
+    "any of its downstream runs, and then continue the resolution from there. Defaults "
+    "to None."
+)
+MAX_PARALLELISM_HELP = (
+    "The maximum number of non-inlined runs that will be allowed to be in the "
+    "`SCHEDULED` state at any one time. Must be a positive integer, or None for "
+    "unlimited runs. Defaults to None."
+)
+INLINE_HELP = (
+    "Whether to include an inline function in the pipeline. Defaults to False."
+)
+NESTED_HELP = "Whether to include nested functions in the pipeline. Defaults to False."
+FAN_OUT_HELP = (
+    "How many dynamically-generated functions to add in parallel. Defaults to 0."
+)
+RAISE_HELP = (
+    "Whether to include a function that raises a ValueError, without retries. "
+    "Defaults to False."
+)
+RAISE_RETRY_HELP = (
+    "Includes a function which raises a ValueError with the given probability, "
+    "with a total of 10 retries. If specified without a value, defaults to 0.5, "
+    "meaning a cumulative probability of complete failure of 0.5 ** 11 = 0.00048828125."
+)
+OOM_HELP = (
+    "Whether to include a function that causes an Out of Memory error. "
+    "Defaults to False."
+)
+EXIT_HELP = (
+    "Includes a function which will exit with the specified code. "
+    "If specified without a value, defaults to 0."
+)
+
+
+def _required_by(*args: str) -> Dict[str, bool]:
+    """Syntactic sugar to specify argparse dependencies between arguments."""
+    return {"required": any([arg in sys.argv for arg in args])}
+
+
+def _parse_args() -> argparse.Namespace:
+    """Parses the command line arguments."""
+    parser = argparse.ArgumentParser(prog=BAZEL_COMMAND, description=DESCRIPTION)
+
+    # Resolver args:
+    parser.add_argument("--log-level", type=str, default="INFO", help=LOG_LEVEL_HELP)
+    parser.add_argument(
+        "--cloud",
+        action="store_true",
+        default=False,
+        help=CLOUD_HELP,
+        **_required_by("--detach", "--max-parallelism", "--oom"),
+    )
+    parser.add_argument(
+        "--detach",
+        action="store_true",
+        default=False,
+        help=DETACH_HELP,
+        **_required_by("--rerun-from"),
+    )
+    parser.add_argument(
+        "--rerun-from",
+        type=str,
+        default=None,
+        help=RERUN_FROM_HELP,
+    )
+    parser.add_argument(
+        "--max-parallelism",
+        type=int,
+        default=None,
+        help=MAX_PARALLELISM_HELP,
+    )
+
+    # Pipeline args:
+    parser.add_argument(
+        "--inline", action="store_true", default=False, help=INLINE_HELP
+    )
+    parser.add_argument(
+        "--nested", action="store_true", default=False, help=NESTED_HELP
+    )
+    parser.add_argument("--fan-out", type=int, default=0, help=FAN_OUT_HELP)
+    parser.add_argument(
+        "--raise",
+        action="store_true",
+        dest="should_raise",
+        default=False,
+        help=RAISE_HELP,
+    )
+    parser.add_argument(
+        "--raise-retry",
+        type=float,
+        nargs="?",
+        const=0.5,
+        default=None,
+        dest="raise_retry_probability",
+        help=RAISE_RETRY_HELP,
+    )
+    parser.add_argument("--oom", action="store_true", default=False, help=OOM_HELP)
+    parser.add_argument(
+        "--exit",
+        type=int,
+        nargs="?",
+        const=0,
+        default=None,
+        dest="exit_code",
+        help=EXIT_HELP,
+    )
+
+    args = parser.parse_args()
+
+    # args values validation
+    logging._checkLevel(args.log_level)
+
+    return args
+
+
+def _get_resolver(args: argparse.Namespace) -> StateMachineResolver:
+    """Instantiates the Resolver based on the passed arguments."""
+    if not args.cloud:
+        return LocalResolver(rerun_from=args.rerun_from)
+
+    return CloudResolver(
+        detach=args.detach,
+        max_parallelism=args.max_parallelism,
+        rerun_from=args.rerun_from,
+    )
+
+
+def main() -> None:
+    args = _parse_args()
+    logging.basicConfig(level=args.log_level)
+    logger.info("Command line arguments: %s", args)
+
+    effective_arg_keys = vars(args).keys() & testing_pipeline.input_types.keys()
+    effective_args = {key: vars(args)[key] for key in effective_arg_keys}
+    logger.info("Pipeline arguments: %s", effective_args)
+
+    resolver = _get_resolver(args)
+    future = testing_pipeline(**effective_args).set(
+        name="Sematic Testing Pipeline", tags=["example", "testing"]
+    )
+
+    logger.info("Invoking the pipeline...")
+    result = future.resolve(resolver)
+    logger.info("Pipeline result: %s", result)
+
+
+if __name__ == "__main__":
+    main()
---
+++ sematic/examples/testing_pipeline/pipeline.py
@@ -0,0 +1,197 @@
+"""
+Flexible-structure pipeline meant to be used in testing.
+"""
+# Standard Library
+import logging
+import os
+import random
+import time
+from typing import List, Optional
+
+# Sematic
+import sematic
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+@sematic.func(inline=False)
+def add(a: float, b: float) -> float:
+    """
+    Adds two numbers.
+    """
+    logger.info("Executing: add(a=%s, b=%s)", a, b)
+    time.sleep(5)
+    return a + b
+
+
+@sematic.func(inline=True)
+def add_inline(a: float, b: float) -> float:
+    """
+    Adds two numbers inline.
+    """
+    logger.info("Executing: add_inline(a=%s, b=%s)", a, b)
+    time.sleep(5)
+    return a + b
+
+
+@sematic.func(inline=False)
+def add2_nested(a: float, b: float) -> float:
+    """
+    Adds two numbers using a nested structure.
+    """
+    logger.info("Executing: add2_nested(a=%s, b=%s)", a, b)
+    return add(a, b)
+
+
+@sematic.func(inline=False)
+def add4_nested(a: float, b: float, c: float, d: float) -> float:
+    """
+    Adds four numbers using a nested structure.
+    """
+    logger.info("Executing: add4_nested(a=%s, b=%s, c=%s, d=%s)", a, b, c, d)
+    return add2_nested(add2_nested(a, b), add2_nested(c, d))
+
+
+@sematic.func(inline=False)
+def add_all(values: List[float]) -> float:
+    """
+    Adds all the numbers in the list.
+    """
+    logger.info("Executing: add_all(values=%s)", values)
+    sum = 0
+    for val in values:
+        sum += val
+    return sum
+
+
+@sematic.func(inline=False)
+def add_fan_out(val: float, fan_out: int) -> float:
+    """
+    Adds the specified number of dynamically-generated functions in parallel.
+    """
+    logger.info("Executing: add_fan_out(val=%s, fan_out=%s)", val, fan_out)
+    futures = []
+    for i in range(fan_out):
+        futures.append(add(val, i))
+    return add_all(futures)
+
+
+@sematic.func(inline=False)
+def do_exit(val: float, exit_code: int) -> float:
+    """
+    Exits execution using the specified exit code.
+
+    The other parameter is ignored.
+    """
+    logger.info("Executing: do_exit(val=%s, exit_code=%s)", val, exit_code)
+    time.sleep(5)
+    os._exit(exit_code)
+    return val
+
+
+@sematic.func(inline=False)
+def do_oom(val: float) -> float:
+    """
+    Causes an Out of Memory error.
+    """
+    logger.info("Executing: do_oom(val=%s)", val)
+    time.sleep(5)
+    m = []
+    while True:
+        m.append(" " * 2**10)
+    return val
+
+
+@sematic.func(inline=False)
+def do_raise(val: float) -> float:
+    """
+    Raises a ValueError, without retries.
+    """
+    logger.info("Executing: do_raise(val=%s)", val)
+    time.sleep(5)
+    raise ValueError("test error")
+
+
+@sematic.func(
+    inline=False, retry=sematic.RetrySettings(exceptions=(ValueError,), retries=10)
+)
+def do_retry(val: float, failure_probability: float = 0.5) -> float:
+    """
+    Raises a ValueError with the given probability, with a total of 10 retries.
+    """
+    logger.info(
+        "Executing: do_retry(val=%s, failure_probability=%s)", val, failure_probability
+    )
+    time.sleep(5)
+    if random.random() < failure_probability:
+        raise ValueError("test retriable exception")
+    return val
+
+
+@sematic.func(inline=True)
+def testing_pipeline(
+    inline: bool = False,
+    nested: bool = False,
+    fan_out: int = 0,
+    should_raise: bool = False,
+    raise_retry_probability: Optional[float] = None,
+    oom: bool = False,
+    exit_code: Optional[int] = None,
+) -> float:
+    """
+    The root function of the testing pipeline.
+
+    Its parameters control the actual shape of the pipeline, according to testing needs.
+
+    Parameters
+    ----------
+    inline: bool
+        Whether to include inline functions in the pipeline. Defaults to False.
+    nested: bool
+        Whether to include nested functions in the pipeline. Defaults to False.
+    fan_out: int
+        How many dynamically-generated functions to add in parallel. Defaults to 0.
+    should_raise: bool
+        Whether to include a function that raises a ValueError, without retries.
+        Defaults to False.
+    raise_retry_probability: Optional[float]
+        If not None, includes a function which raises a ValueError with the given
+        probability, with a total of 10 retries. Defaults to None.
+    oom: bool
+        Whether to include a function that causes an Out of Memory error.
+        Defaults to False.
+    exit_code: Optional[int]
+        If not None, includes a function which will exit with the specified code.
+        Defaults to None.
+    """
+    # have an initial function whose output is used as inputs by all other functions
+    # this staggers the rest of the functions and allows the user a change to monitor and
+    # visualize the unfolding execution
+    initial_future = add(1, 2)
+    futures = [initial_future]
+
+    if inline:
+        futures.append(add_inline(initial_future, 3))
+
+    if nested:
+        futures.append(add4_nested(initial_future, 1, 2, 3))
+
+    if fan_out > 0:
+        futures.append(add_fan_out(initial_future, fan_out))
+
+    if should_raise:
+        futures.append(do_raise(initial_future))
+
+    if raise_retry_probability:
+        futures.append(do_retry(initial_future))
+
+    if oom:
+        futures.append(do_oom(initial_future))
+
+    if exit_code is not None:
+        futures.append(do_exit(initial_future, exit_code))
+
+    # collect all values
+    result = add_all(futures) if len(futures) > 1 else futures[0]
+    return result
