20486aa482f614afff85ee4b7adf64b6756824db
augray <augray@users.noreply.github.com>
2022-11-10 12:36:59 -0800

    Add sqlite version check (#314)

--- sematic/versions.py
+++ sematic/versions.py
@@ -1,4 +1,5 @@
 # Standard Library
+import sqlite3
 import typing
 
 # Represents the version of the client, server, and all other parts of
@@ -11,6 +12,23 @@ CURRENT_VERSION = (0, 19, 2)
 # at the CURRENT_VERSION. Should be updated any time a breaking change
 # is made to the web API.
 MIN_CLIENT_SERVER_SUPPORTS = (0, 19, 0)
+
+# Support for dropping columns added in 3.35.0
+MIN_SQLITE_VERSION = (3, 35, 0)
+
+
+def _assert_sqlite_version():
+    version_tuple = sqlite3.sqlite_version.split(".")
+
+    # get major/minor as ints. Patch can sometimes have non-digit chars
+    major, minor = int(version_tuple[0]), int(version_tuple[1])
+    if (major, minor) < MIN_SQLITE_VERSION:
+        raise RuntimeError(
+            f"Sematic requires sqlite3 version to be at least "
+            f"{version_as_string(MIN_SQLITE_VERSION)}, but your python is using "
+            f"{sqlite3.sqlite_version}. Please upgrade. You may find this useful: "
+            f"https://stackoverflow.com/a/55729735/2540669"
+        )
 
 
 def version_as_string(version: typing.Tuple[int, int, int]) -> str:
@@ -30,7 +48,7 @@ def version_as_string(version: typing.Tuple[int, int, int]) -> str:
 
 CURRENT_VERSION_STR = version_as_string(CURRENT_VERSION)
 MIN_CLIENT_SERVER_SUPPORTS_STR = version_as_string(MIN_CLIENT_SERVER_SUPPORTS)
-
+_assert_sqlite_version()
 
 if __name__ == "__main__":
     # It can be handy for deployment scripts and similar things to be able to get quick
