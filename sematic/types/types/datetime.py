2f2c8584747598d8ff9d86b10effb2116d19ee67
Erik Kandalík <32076170+erikcek@users.noreply.github.com>
2022-11-14 23:21:49 +0100

    Add datetime class and basic visualization (#220)
    * Add datetime class and basic visualization
    
    - add datetime class to Types.tsx
    - add DatetimeValueView for visualization
    - add Localization provider for DatetimeValueView
    
    * Review improvements
    
    - use python native datetime class for date representation
    - update tests
    - remove MUI datepicker for datetime visualization
    
    * Review improvements
    
    - change date format
    - fix tests
    - revert unintended changes

---
+++ sematic/types/types/datetime.py
@@ -0,0 +1,29 @@
+"""
+Datetime lets user display native python datetime object in the UI with specified
+formatting
+"""
+# Standard Library
+import typing
+from datetime import datetime
+
+# Sematic
+from sematic.types.registry import (
+    register_from_json_encodable,
+    register_to_json_encodable,
+    register_to_json_encodable_summary,
+)
+
+
+@register_to_json_encodable_summary(datetime)
+def _datetime_summary(value: datetime, _) -> str:
+    return value.isoformat()
+
+
+@register_to_json_encodable(datetime)
+def _datetime_to_encodable(value: datetime, type_: typing.Type[datetime]) -> str:
+    return value.isoformat()
+
+
+@register_from_json_encodable(datetime)
+def _datetime_from_encodable(value: str, type_: typing.Type[datetime]) -> datetime:
+    return datetime.fromisoformat(value)
