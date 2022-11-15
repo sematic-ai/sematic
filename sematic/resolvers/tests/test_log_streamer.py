580b3a953b5b3cab2c6e875cd8e4b83c70ceec6f
augray <augray@users.noreply.github.com>
2022-11-15 09:44:16 -0800

    Change log-ingestion to use deltas rather than full files (#306)
    * Switch to incremental uploads
    
    * simplify tail
    
    * Cleaning
    
    * remove -f since it no longer applies
    
    * Review comments

--- sematic/resolvers/tests/test_log_streamer.py
+++ sematic/resolvers/tests/test_log_streamer.py
@@ -1,17 +1,24 @@
 # Standard Library
+import os
 import pathlib
+import signal
 import sys
 import tempfile
 import time
 
 # Sematic
-from sematic.resolvers.log_streamer import _tail_log_file, ingested_logs
+from sematic.resolvers.log_streamer import (
+    _send_signal_or_kill,
+    _start_log_streamer_out_of_process,
+    _tail_log_file,
+    ingested_logs,
+)
 from sematic.utils.retry import retry
 
 
 @retry(AssertionError, tries=3)  # this test is somewhat dependent on relative timings.
 def test_ingested_logs():
-    with tempfile.NamedTemporaryFile() as log_file:
+    with tempfile.NamedTemporaryFile(delete=False) as log_file:
         remote_prefix = "foo/bar"
         upload_interval = 0.1
         with ingested_logs(
@@ -37,25 +44,68 @@ def test_ingested_logs():
 
     assert len(uploads) > 1  # should be at least one timed, and the final flush
 
-    # early uploads should have only contained early writes
-    assert any(
-        upload
-        == [
-            f"{remote_prefix}\n",
-            "From stdout\n",
-            "From stderr\n",
-        ]
-        for upload in uploads
-    )
-
-    # last upload should have everything
-    assert uploads[-1] == [
-        f"{remote_prefix}\n",
+    everything = [
         "From stdout\n",
         "From stderr\n",
         "From stdout late\n",
         "From stderr late\n",
     ]
+
+    # early uploads should have only early stuff, late uploads should
+    # have late stuff. So no single upload should have everything.
+    assert not any(upload == [f"{remote_prefix}\n"] + everything for upload in uploads)
+
+    all_upload_contents = concat_uploads(uploads)
+
+    # everything uploaded should contain all log lines, with
+    # no duplicated lines.
+    assert all_upload_contents == everything
+
+
+@retry(AssertionError, tries=3)  # this test is somewhat dependent on relative timings.
+def test_start_log_streamer_out_of_process():
+    remote_prefix = "foo/bar"
+    upload_interval = 100
+    started = time.time()
+    with tempfile.NamedTemporaryFile(delete=False) as log_file:
+        read_file_descriptor, write_file_descriptor = os.pipe()
+        os.set_blocking(read_file_descriptor, False)
+        os.set_inheritable(read_file_descriptor, True)
+        child_pid = _start_log_streamer_out_of_process(
+            file_path=log_file.name,
+            read_from_file_descriptor=read_file_descriptor,
+            upload_interval_seconds=upload_interval,
+            remote_prefix=remote_prefix,
+            uploader=mock_uploader,
+        )
+        write_handle = os.fdopen(write_file_descriptor, "w")
+        contents = "Hello, child!"
+        write_handle.write(contents)
+        write_handle.flush()
+
+        # give some time for the process to actually start up
+        # and register a handler
+        time.sleep(1)
+        _send_signal_or_kill(child_pid, signal.SIGTERM, timeout_seconds=5)
+
+    assert time.time() - started < upload_interval
+    try:
+        os.kill(child_pid, 0)
+    except OSError:
+        # Child should have exited
+        pass
+    else:
+        assert False, "Child process should have exited"
+
+    upload_number = 0
+    uploads = []
+    while pathlib.Path(_upload_path(log_file.name, upload_number)).exists():
+        with open(_upload_path(log_file.name, upload_number), "r") as upload:
+            uploads.append(list(upload))
+        upload_number += 1
+
+    assert len(uploads) >= 1
+    assert contents in "\n".join(concat_uploads(uploads))
 
 
 def test_ingested_logs_uncaught():
@@ -63,7 +113,7 @@ def test_ingested_logs_uncaught():
     started = time.time()
     message = "Uncaught error!"
     try:
-        with tempfile.NamedTemporaryFile() as log_file:
+        with tempfile.NamedTemporaryFile(delete=False) as log_file:
             remote_prefix = "foo/bar"
             upload_interval = 100  # should exit early despite the long interval
             with ingested_logs(
@@ -86,7 +136,7 @@ def test_ingested_logs_uncaught():
         upload_number += 1
 
     assert len(uploads) >= 1
-    assert message in "\n".join(uploads[-1])
+    assert message in "\n".join(concat_uploads(uploads))
 
 
 def test_tail_log_file():
@@ -95,7 +145,7 @@ def test_tail_log_file():
     def print_func(to_print, end="\n", **kwargs):
         printed.append(to_print + end)
 
-    with tempfile.NamedTemporaryFile() as log_file:
+    with tempfile.NamedTemporaryFile(delete=False) as log_file:
         remote_prefix = "foo/bar"
         upload_interval = 0.1
         line_template = "This is line {}"
@@ -110,32 +160,35 @@ def test_tail_log_file():
             for i in range(n_lines):
                 print(line_template.format(i))
                 print(stderr_line_template.format(i), file=sys.stderr)
-        _tail_log_file(log_file.name, max_tail_bytes=0, print_func=print_func)
-
-        assert len(printed) == 0
-        _tail_log_file(
-            log_file.name, max_tail_bytes=6 * len(line_template), print_func=print_func
-        )
+        _tail_log_file(log_file.name, print_func=print_func)
+        _tail_log_file(log_file.name, print_func=print_func)
         assert len(printed) > 0
-        assert printed[-1] == stderr_line_template.format(n_lines - 1) + "\n"
-        assert printed[-2] == line_template.format(n_lines - 1) + "\n"
         has_ellipsis = any(
             line.replace("\n", "").replace("\t", "") == "..." for line in printed
         )
         assert has_ellipsis
 
-        printed.clear()
+        assert printed[-1].replace("\n", "") == stderr_line_template.format(n_lines - 1)
 
-        # all lines present
-        _tail_log_file(log_file.name, max_tail_bytes=2**30, print_func=print_func)
-        assert printed[-2 * n_lines] == line_template.format(0) + "\n"
-        assert printed[-2 * n_lines + 1] == stderr_line_template.format(0) + "\n"
-        assert printed[-2] == line_template.format(n_lines - 1) + "\n"
-        assert printed[-1] == stderr_line_template.format(n_lines - 1) + "\n"
-        has_ellipsis = any(
-            line.replace("\n", "").replace("\t", "") == "..." for line in printed
-        )
-        assert not has_ellipsis
+
+def test_tail_log_file_empty():
+    printed = []
+
+    def print_func(to_print, end="\n", **kwargs):
+        printed.append(to_print + end)
+
+    with tempfile.NamedTemporaryFile(delete=False) as log_file:
+        remote_prefix = "foo/bar"
+        upload_interval = 0.1
+        with ingested_logs(
+            log_file.name,
+            upload_interval_seconds=upload_interval,
+            remote_prefix=remote_prefix,
+            uploader=mock_uploader,
+        ):
+            pass
+        _tail_log_file(log_file.name, print_func=print_func)
+        assert printed[-1] == "<No lines in latest delta files>\n"
 
 
 def _upload_path(file_uploaded, upload_number):
@@ -156,3 +209,12 @@ def mock_uploader(file_path, remote_prefix):
         with open(file_path, "rb") as log:
             for line in log:
                 record.write(line)
+
+
+def concat_uploads(uploads):
+    all_upload_contents = []
+    for upload in uploads:
+        all_upload_contents.extend(
+            upload[1:]
+        )  # 1st line of each upload is remote prefix
+    return all_upload_contents
--- sematic/utils/tests/test_stdout.py
+++ sematic/utils/tests/test_stdout.py
@@ -1,9 +1,10 @@
 # Standard Library
+import os
 import sys
 import tempfile
 
 # Sematic
-from sematic.utils.stdout import redirect_to_file
+from sematic.utils.stdout import redirect_to_file, redirect_to_file_descriptor
 
 
 def test_redirect_to_file():
@@ -25,4 +26,32 @@ def test_redirect_to_file():
         "from stderr",
         # "from subprocess",  # see comment above
         "during2",
+    ]
+
+
+def test_redirect_to_file_descriptor():
+    read_fd, write_fd = os.pipe()
+    read_handle = os.fdopen(read_fd)
+    os.set_blocking(read_fd, False)
+    print("before")
+    with redirect_to_file_descriptor(write_fd):
+        print("during1")
+        print("from stderr1", file=sys.stderr)
+        print("during2")
+        print("from stderr2", file=sys.stderr)
+        print("from stderr3", file=sys.stderr)
+        line = None
+        lines = []
+        while line is None or len(line) > 0:
+            line = read_handle.readline()
+            if len(line) > 0:
+                lines.append(line)
+    print("after")
+
+    assert lines == [
+        "during1\n",
+        "from stderr1\n",
+        "during2\n",
+        "from stderr2\n",
+        "from stderr3\n",
     ]
