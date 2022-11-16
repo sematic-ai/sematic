96bf87e74b86b092098cff813bdf153ee221b78c
augray <apps@joshbauer.email>
2022-11-16 14:38:13 -0800

    fix cursor seek

--- sematic/log_reader.py
+++ sematic/log_reader.py
@@ -433,6 +433,12 @@ def _line_stream_from_log_directory(
         if log_file == cursor_file:
             found_cursor_file = True
         if not found_cursor_file:
+            logger.info(
+                "On file %s, haven't found cursor %s in %s",
+                log_file,
+                cursor_file,
+                log_files,
+            )
             continue
         text_stream: Iterable[str] = S3Storage().get_line_stream(log_file)
         for i_line, line in enumerate(text_stream):
@@ -441,8 +447,15 @@ def _line_stream_from_log_directory(
                 and cursor_line_index is not None
                 and i_line < cursor_line_index
             ):
+                logger.info(
+                    "On line %s in %s, haven't found cursor line %s",
+                    i_line,
+                    log_file,
+                    cursor_line_index,
+                )
                 continue
             found_cursor_line = True
+            logger.info("Yielding line %s in %s: %s", i_line, log_file, line)
             yield LogLine(
                 source_file=log_file,
                 source_file_index=i_line,
@@ -581,6 +594,7 @@ def get_log_lines_from_line_stream(
             if not found_cursor:
                 if (
                     cursor_source_file is None
+                    or source_file > cursor_source_file
                     or source_file == cursor_source_file
                     and source_file_line_index >= cursor_line_index
                 ):
@@ -590,6 +604,7 @@ def get_log_lines_from_line_stream(
                     continue
 
             if not passes_filter(line):
+                logger.info("Line doesn't pass filters: %s", line)
                 continue
 
             lines.append(line.line)
@@ -598,6 +613,7 @@ def get_log_lines_from_line_stream(
                 has_more = True
                 keep_going = False
         except StopIteration:
+            logger.info("Out of lines: %s")
             keep_going = False
 
             # hit the end of the logs produced so far. If the run is
@@ -605,7 +621,7 @@ def get_log_lines_from_line_stream(
             # up!
             has_more = still_running
     missing_reason = None if len(lines) > 0 else "No matching log lines."
-    return LogLineResult(
+    result = LogLineResult(
         more_before=more_before,
         more_after=has_more,
         lines=lines,
@@ -621,3 +637,5 @@ def get_log_lines_from_line_stream(
         else None,
         log_unavailable_reason=missing_reason,
     )
+    logger.info("Returning %s", result)
+    return result
