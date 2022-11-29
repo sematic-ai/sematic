df6e0fad7d5881de0708b13e5a9f7694e13eb9df
Chance An <anchangsi@gmail.com>
2022-11-29 21:40:20 +0800

    Chance/refactor view redirection (#357)
    Now we have a dedicated page to handle `pipelines/:calculatorPath`
    routes. Therefore the process of delegating a sub-component to find the
    correct run and redirect URL could be lifted to a higher level. This
    saves some unnecessary communication across component layers.
    
    This is also more consistent with the idea of deep linking, in that we
    grant different URL patterns different semantics.
    `pipelines/:calculatorPath` then has its own purpose and which does not
    overlap with the semantic of `pipelines/:calculatorPath/:rootId`.
    
    Co-authored-by: tscurtu <tudor@sematic.dev>

---
+++ sematic/ui/src/pipelines/PipelineRunView.tsx
@@ -0,0 +1,97 @@
+import Box from "@mui/material/Box";
+import { useCallback, useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { UserContext } from "..";
+import PipelineBar from "../components/PipelineBar";
+import PipelinePanels from "../components/PipelinePanels";
+import { Resolution, Run } from "../Models";
+import { ResolutionPayload, RunViewPayload } from "../Payloads";
+import { fetchJSON } from "../utils";
+
+export default function PipelineRunView() {
+  const [rootRun, setRootRun] = useState<Run | undefined>(undefined);
+  const [resolution, setResolution] = useState<Resolution | undefined>(
+    undefined
+  );
+
+  const { user } = useContext(UserContext);
+
+  const params = useParams();
+
+  const { calculatorPath, rootId } = params;
+
+  const fetchRootRun = useCallback(
+    (rootId: string) => {
+      fetchJSON({
+        url: "/api/v1/runs/" + rootId,
+        apiKey: user?.api_key,
+        callback: (payload: RunViewPayload) => setRootRun(payload.content),
+      });
+    },
+    [setRootRun, user?.api_key]
+  );
+
+  const fetchResolution = useCallback(
+    (rootId: string) => {
+      fetchJSON({
+        url: "/api/v1/resolutions/" + rootId,
+        apiKey: user?.api_key,
+        callback: (payload: ResolutionPayload) =>
+          setResolution(payload.content),
+      });
+    },
+    [setResolution, user?.api_key]
+  );
+
+  useEffect(() => {
+    fetchRootRun(rootId!);
+    fetchResolution(rootId!);
+  }, [rootId, fetchRootRun, fetchResolution]);
+
+  const changeRootRunId = useCallback(
+    (rootId: string) => {
+      if (rootRun && rootRun.id === rootId) return;
+      var runURL =
+        window.location.protocol +
+        "//" +
+        window.location.host +
+        "/pipelines/" +
+        calculatorPath +
+        "/" +
+        rootId;
+      window.history.pushState({ path: runURL }, "", runURL);
+      fetchRootRun(rootId);
+      fetchResolution(rootId);
+    },
+    [calculatorPath, rootRun, fetchRootRun, fetchResolution]
+  );
+
+  if (calculatorPath) {
+    return (
+      <Box
+        sx={{
+          display: "grid",
+          gridTemplateColumns: "250px 1fr 350px",
+          gridTemplateRows: "70px 1fr",
+          height: "100vh",
+        }}
+      >
+        <PipelineBar
+          calculatorPath={calculatorPath}
+          onRootIdChange={changeRootRunId}
+          rootRun={rootRun}
+          resolution={resolution}
+        />
+        {rootRun && resolution && (
+          <PipelinePanels
+            rootRun={rootRun}
+            resolution={resolution}
+            onRootRunUpdate={setRootRun}
+            onRootIdChange={changeRootRunId}
+          />
+        )}
+      </Box>
+    );
+  }
+  return <></>;
+}
