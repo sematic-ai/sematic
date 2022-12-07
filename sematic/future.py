8dc511163d68c9ee464433d0a50fb9317d370bf1
tscurtu <tudor@sematic.dev>
2022-12-07 12:53:17 +0200

    Update Future docstrings and properties validation (#381)
    Updates docstrings and validation code in locations that will be touched
    by implementation of the Function Output Artifact Caching feature
    (#338).
    
    Extracted out as a separate PR in order to reduce the overall size of
    the PRs that will implement that feature.
    
    Also updated the unit tests and added new test cases to completely cover
    the future properties validation.

--- sematic/future.py
+++ sematic/future.py
@@ -1,16 +1,17 @@
 # Standard Library
-import typing
+from typing import Any, List, Optional  # noqa: F401
 
 # Sematic
 from sematic.abstract_future import AbstractFuture, FutureState
 from sematic.resolver import Resolver
 from sematic.resolvers.local_resolver import LocalResolver
+from sematic.resolvers.resource_requirements import ResourceRequirements
 from sematic.resolvers.silent_resolver import SilentResolver
 
 
 class Future(AbstractFuture):
     """
-    Class representing a future.
+    Class representing a future function execution.
 
     A future is essentially a tuple of with two elements:
 
@@ -19,13 +20,15 @@ class Future(AbstractFuture):
     """
 
     def resolve(
-        self, resolver: typing.Optional[Resolver] = None, tracking: bool = True
-    ) -> typing.Any:
+        self, resolver: Optional[Resolver] = None, tracking: bool = True
+    ) -> Any:
         """
         Trigger the resolution of the future and all its nested futures.
 
         Parameters
         ----------
+        resolver: Optional[Resolver]
+            The `Resolver` to use to execute this future.
         tracking: bool
             Enable tracking. Defaults to `True`. If `True`, the future's
             execution as well as that of all its nested future will be tracked
@@ -48,40 +51,66 @@ class Future(AbstractFuture):
         ----------
         name: str
             The future's name. This will be used to name the run in the UI.
-        tags: typing.List[str]
+
+        inline: bool
+            When using the `CloudResolver`, whether the instrumented function
+            should be executed inside the same process and worker that is executing
+            the `Resolver` itself.
+
+            Defaults to `True`, as most pipeline functions are expected to be
+            lightweight. Explicitly set this to `False` in order to distribute its
+            execution to a worker and parallelize its execution.
+
+        resource_requirements: ResourceRequirements
+            When using the `CloudResolver`, specifies what special execution
+            resources the function requires. Defaults to `None`.
+
+        tags: List[str]
             A list of strings tags to attach to the resulting run.
 
         Returns
         -------
         Future
-            The current future. This enables chaining.
+            The current `Future` object. This enables chaining.
         """
-        mutable_fields = {"name", "inline", "tags", "resource_requirements"}
+        mutable_fields = {"name", "inline", "resource_requirements", "tags"}
         invalid_fields = set(kwargs) - mutable_fields
         if len(invalid_fields) > 0:
-            raise ValueError("Cannot mutate fields: {}".format(invalid_fields))
+            raise ValueError(f"Cannot mutate fields: {invalid_fields}")
 
+        if "name" in kwargs:
+            value = kwargs["name"]
+            if not (isinstance(value, str) and len(value) > 0):
+                raise ValueError(
+                    f"Invalid `name`, must be a non-empty string: {repr(value)}"
+                )
+
+        if "inline" in kwargs:
+            value = kwargs["inline"]
+            if not (isinstance(value, bool)):
+                raise ValueError(f"Invalid `inline`, must be a bool: {repr(value)}")
+
+        if "resource_requirements" in kwargs:
+            value = kwargs["resource_requirements"]
+            if not (isinstance(value, ResourceRequirements)):
+                raise ValueError(
+                    f"Invalid `resource_requirements`, must be a ResourceRequirements: "
+                    f"{repr(value)}"
+                )
+
+        if "tags" in kwargs:
+            value = kwargs["tags"]
+            if not (
+                isinstance(value, list)
+                and all(isinstance(tag, str) and len(tag) > 0 for tag in value)
+            ):
+                raise ValueError(
+                    f"Invalid `tags`, must be a list of non empty strings: {repr(value)}"
+                )
+
+        # at this point, kwargs contains only mutable properties,
+        # and they have all been checked
         for name, value in kwargs.items():
-            if name == "tags":
-                if not (
-                    isinstance(value, list)
-                    and all(isinstance(tag, str) and len(tag) > 0 for tag in value)
-                ):
-                    raise ValueError(
-                        "Invalid tags, must a list of non empty strings: {}".format(
-                            repr(value)
-                        )
-                    )
-
-            if name == "name":
-                if not (isinstance(value, str) and len(value) > 0):
-                    raise ValueError(
-                        "Invalid name, must be a non-empty string: {}".format(
-                            repr(value)
-                        )
-                    )
-
-            # TODO: valdidate inline
             setattr(self._props, name, value)
 
         return self
