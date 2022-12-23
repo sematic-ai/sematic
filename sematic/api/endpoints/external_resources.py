4dbeb0ecb2343317c0c79df361548cc4dd189f5d
augray <apps@joshbauer.email>
2022-12-23 10:08:13 -0800

    Move endpoint

--- sematic/api/endpoints/external_resources.py
+++ sematic/api/endpoints/external_resources.py
@@ -15,35 +15,9 @@ from sematic.db.models.external_resource import (
 from sematic.db.models.user import User
 from sematic.db.queries import (
     get_external_resource_record,
-    get_resource_ids_by_root_id,
     save_external_resource_record,
     save_run_external_resource_link,
 )
-
-ROOT_ID_KEY = "root_id"
-
-
-# Allow getting a list of external resource ids.
-# Q: Why not just return the actual resources?
-# A: Because we should have the endpoint for getting an external resource
-#    hooked up such that it actually interacts with the external compute
-#    to get the most up-to-date status of the records. It would be too much work
-#    for one service call to have to do that for a *list* of resources.
-# Q: Ok, then why not just return the resources without updating them for this?
-# A: Because it would be weird to have the server return stale states when queried
-#    in a list but up-to-date ones when queried one-by-one.
-@sematic_api.route("/api/v1/external_resources/ids", methods=["GET"])
-@authenticate
-def get_resources_endpoint(user: Optional[User]) -> flask.Response:
-    if set(flask.request.args.keys()) != {ROOT_ID_KEY}:
-        return jsonify_error(
-            f"Can only get external resources using the query key {ROOT_ID_KEY} ",
-            HTTPStatus.BAD_REQUEST,
-        )
-
-    resource_ids = get_resource_ids_by_root_id(flask.request.args[ROOT_ID_KEY])
-    payload = dict(resource_ids=resource_ids)
-    return flask.jsonify(payload)
 
 
 @sematic_api.route("/api/v1/external_resources/<resource_id>", methods=["GET"])
