f45e75e18839d62af71ee93781d5ceaf361e3351
Emmanuel Turlay <emmanuel@sematic.ai>
2022-12-23 15:15:00 -0800

    [2/N] Storage plug-ins: `MemoryStorage`, `LocalStorage`, and `S3Storage` (#407)
    Introducing three storage plugins for server-prescribed upload
    locations.
    
    The plug-ins are not yet used in the code, this PR only creates them.

---
+++ sematic/plugins/storage/tests/test_local_storage.py
@@ -0,0 +1,34 @@
+# Standard Library
+import tempfile
+from unittest import mock
+
+# Third-party
+import flask.testing
+
+# Sematic
+from sematic.api.tests.fixtures import mock_auth, test_client  # noqa: F401
+from sematic.db.tests.fixtures import test_db  # noqa: F401
+from sematic.plugins.storage.local_storage import LocalStorage
+
+
+def test_upload_download(
+    mock_auth,  # noqa: F811
+    test_client: flask.testing.FlaskClient,  # noqa: F811
+):
+    value = b"foo"
+
+    local_storage = LocalStorage()
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        with mock.patch(
+            "sematic.plugins.storage.local_storage._get_data_dir", return_value=tempdir
+        ):
+            response = test_client.put(
+                local_storage.get_write_location("artifacts", "123"), data=value
+            )
+
+            assert response.status_code == 200
+
+            read_payload = local_storage.get_read_payload("artifacts", "123")
+
+            assert read_payload.content == value
