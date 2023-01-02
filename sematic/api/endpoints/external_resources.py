e59a4d642423dbd2982b331937cad39c4c5eced7
augray <apps@joshbauer.email>
2023-01-02 08:55:01 -0800

    endpoints review

--- sematic/api/endpoints/external_resources.py
+++ sematic/api/endpoints/external_resources.py
@@ -16,7 +16,6 @@ from sematic.db.models.user import User
 from sematic.db.queries import (
     get_external_resource_record,
     save_external_resource_record,
-    save_run_external_resource_link,
 )
 
 
@@ -29,41 +28,26 @@ def get_resource_endpoint(user: Optional[User], resource_id: str) -> flask.Respo
             "No such resource: {}".format(resource_id), HTTPStatus.NOT_FOUND
         )
 
-    payload = dict(record=record.to_json_encodable())
+    payload = dict(external_resource=record.to_json_encodable())
 
     return flask.jsonify(payload)
 
 
-@sematic_api.route("/api/v1/external_resources/<resource_id>", methods=["POST"])
+@sematic_api.route("/api/v1/external_resources", methods=["POST"])
 @authenticate
-def save_resource_endpoint(user: Optional[User], resource_id: str) -> flask.Response:
+def save_resource_endpoint(user: Optional[User]) -> flask.Response:
     if (
         not flask.request
         or not flask.request.json
-        or "record" not in flask.request.json
+        or "external_resource" not in flask.request.json
     ):
-        return jsonify_error("Request should have 'record' key", HTTPStatus.BAD_REQUEST)
-
-    record_json_encodable = flask.request.json["record"]
-    record = ExternalResourceRecord.from_json_encodable(record_json_encodable)
-
-    if record.id != resource_id:
         return jsonify_error(
-            "Resource id should match serialized resource record",
-            HTTPStatus.BAD_REQUEST,
+            "Request should have 'external_resource' key", HTTPStatus.BAD_REQUEST
         )
+
+    record_json_encodable = flask.request.json["external_resource"]
+    record = ExternalResourceRecord.from_json_encodable(record_json_encodable)
     record = save_external_resource_record(record)
-    payload = dict(record=record.to_json_encodable())
+    payload = dict(external_resource=record.to_json_encodable())
 
     return flask.jsonify(payload)
-
-
-@sematic_api.route(
-    "/api/v1/external_resources/<resource_id>/linked_run/<run_id>", methods=["POST"]
-)
-@authenticate
-def save_resource_run_link_endpoint(
-    user: Optional[User], resource_id: str, run_id: str
-) -> flask.Response:
-    save_run_external_resource_link(resource_id, run_id)
-    return flask.jsonify({"run_id": run_id, "resource_id": resource_id})
