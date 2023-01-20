575f568f053079204d3694959ffd96ca59de2cd7
augray <augray@users.noreply.github.com>
2023-01-20 12:08:20 -0500

    Kuberay wrapper plugin (#467)
    We will need a way to convert between a "user-oriented" way to specify a
    Ray cluster (python dataclass object, describing in terms of cpu counts,
    memory, gpu information), and an "infra oriented" way (a Kuberay K8s CRD
    for a RayCluster). This PR provides an implementation of a conversion
    between these two representations. Because there are a wide variety of
    ways this conversion could be done, a wide variety of ways to configure
    the RayCluster CRDs, and even some instability in the structure of the
    CRDs between different Kuberay versions, this conversion is done via a
    server-side plugin. A default plugin implementation is provided, which
    should cover people using the most recent Kuberay and a conventional K8s
    setup for GPU access.
    
    With respect to GPU access, the default implementation provided will
    support GPU usage via plugin configurations. All the plugin
    configurations should be provided as json encoded into strings. The
    configurations include:
    - turning on GPU support (boolean flag)
    - allowing specifying different tolerations for Ray workers/head nodes
    that leverage GPUs
    - allowing specifying different node selectors for Ray workers/head
    nodes that leverage GPUs
    - allowing specifying a key to use in resource requests/limits for GPU
    count
    
    node selectors and tolerations are also configurable for non-GPU
    workers/heads. We know that existing users provide GPU support via
    tolerations & node selectors.

---
+++ sematic/plugins/abstract_kuberay_wrapper.py
@@ -0,0 +1,183 @@
+# Standard Library
+from dataclasses import dataclass, field
+from typing import Any, Dict, List, Optional, Tuple
+
+# Sematic
+from sematic.abstract_plugin import AbstractPlugin
+
+# This should be the manifest that can be passed to the
+# Kubernetes API for the RayCluster CRD here:
+# https://github.com/ray-project/kuberay/blob/master/helm-chart/kuberay-operator/crds/ray.io_rayclusters.yaml
+RayClusterManifest = Dict[str, Any]
+
+
+@dataclass(frozen=True)
+class RayNodeConfig:
+    """Configuration for an individual Ray Head/Worker compute node.
+
+    Custom implementations of AbstractKuberayWrapper that allow for
+    more advanced configuration (e.g. selecting among multiple kinds
+    of GPU) can subclass this class and add extra configuration fields.
+
+    Attributes
+    ----------
+    cpu:
+        Number of CPUs for each node (supports fractional CPUs).
+    memory_gb:
+        Gigabytes of memory for each node (supports fractional values).
+    gpu_count:
+        The number of GPUs to attach. Not all deployments support GPUs.
+    """
+
+    cpu: float
+    memory_gb: float
+    gpu_count: int = 0
+
+
+@dataclass(frozen=True)
+class ScalingGroup:
+    """Configuration for a group of Ray workers that will scale as a unit.
+
+    Attributes
+    ----------
+    worker_nodes:
+        A description of the compute resources available for each node in the
+        scaling group.
+    min_workers:
+        The minimum number of workers the scaling group can scale to. Must be
+        non-negative.
+    max_workers:
+        The maximum number of workers the scaling group can scale to. Must be
+        equal to or greater than min_workers. For a fixed-size scaling group,
+        set this equal to min_workers.
+    """
+
+    worker_nodes: RayNodeConfig
+    min_workers: int = 1
+    max_workers: int = 1
+
+    def __post_init__(self):
+        if self.min_workers <= 0:
+            raise ValueError("min_workers must be >= 1")
+        if self.min_workers > self.max_workers:
+            raise ValueError("max_workers must be >= min_workers")
+
+
+def _get_ray_version() -> str:
+    """Default factory for the field ray_version in RayClusterConfig."""
+    try:
+        # Third-party
+        import ray  # type: ignore
+
+        return ray.__version__
+    except ImportError:
+        raise ValueError(
+            "If ray is not installed, a value must be provided for ray_version"
+        )
+
+
+@dataclass(frozen=True)
+class RayClusterConfig:
+    """Description of a Ray Cluster
+
+    Attributes
+    ----------
+    head_node:
+        The configuration for the head node
+    scaling_groups:
+        A list of scaling groups. Each scaling group may have different
+        properties for the nodes in the group.
+    ray_version:
+        The version of Ray the cluster should use. This will be populated
+        automatically to the version of Ray currently installed, if Ray
+        is already installed
+    """
+
+    head_node: RayNodeConfig
+    scaling_groups: List[ScalingGroup] = field(default_factory=list)
+    ray_version: str = field(default_factory=_get_ray_version)
+
+
+def SimpleRayCluster(
+    n_nodes: int, node_config: RayNodeConfig, ray_version: Optional[str] = None
+) -> RayClusterConfig:
+    """Configuration for a RayCluster with a fixed number of identical compute nodes
+
+    Parameters
+    ----------
+    n_nodes:
+        The number of nodes in the cluster, including the head node
+    node_config:
+        The configuration for each node in the cluster
+    ray_version:
+        The version of Ray used by the cluster. Will be populated automatically
+        if Ray is installed. Otherwise it must be explicitly configured.
+    """
+    if ray_version is None:
+        ray_version = _get_ray_version()
+    if n_nodes < 1:
+        raise ValueError("There must be at least one node in the Ray Cluster")
+    n_workers = n_nodes - 1
+    scaling_groups = []
+    if n_workers > 0:
+        scaling_groups.append(
+            ScalingGroup(
+                worker_nodes=node_config,
+                min_workers=n_workers,
+                max_workers=n_workers,
+            )
+        )
+    return RayClusterConfig(
+        head_node=node_config,
+        scaling_groups=scaling_groups,
+        ray_version=ray_version,
+    )
+
+
+class AbstractKuberayWrapper(AbstractPlugin):
+    """Plugin to convert between a RayClusterConfig & a k8s manifest for the cluster."""
+
+    @staticmethod
+    def get_author() -> str:
+        return "github.com/sematic-ai"
+
+    @staticmethod
+    def get_version() -> Tuple[int, int, int]:
+        return 0, 1, 0
+
+    @classmethod
+    def create_cluster_manifest(
+        cls,
+        image_uri: str,
+        cluster_name: str,
+        cluster_config: RayClusterConfig,
+        kuberay_version: str,
+    ) -> RayClusterManifest:
+        """Create a kubernetes manifest for a Ray cluster.
+
+        Parameters
+        ----------
+        image_uri:
+            The docker image that should be used by the RayCluster
+        cluster_name:
+            The name that the RayCluster object should have
+        cluster_config:
+            Specifications on what the RayCluster should look like
+        kuberay_version:
+            A Sematic version tuple for the installed Kuberay
+
+        Returns
+        -------
+        A json-encodable for the RayCluster kubernetes object described by this CRD:
+        https://github.com/ray-project/kuberay/blob/master/helm-chart/kuberay-operator/crds/ray.io_rayclusters.yaml
+
+        Raises
+        ------
+        UnsupportedVersionError:
+            If the provided version of Kuberay is not one that's supported by this plugin.
+        UnsupportedUsageError:
+            If GPUs are requested but the plugin doesn't support configuring for GPUs.
+        """
+        raise NotImplementedError(
+            "Child classes should implement create_cluster_manifest"
+        )
---
+++ sematic/plugins/kuberay_wrapper/standard.py
@@ -0,0 +1,345 @@
+# Standard Library
+import json
+from copy import deepcopy
+from typing import Any, Dict, Type
+
+# Sematic
+from sematic.abstract_plugin import AbstractPluginSettingsVar
+from sematic.config.settings import get_plugin_setting
+from sematic.plugins.abstract_kuberay_wrapper import (
+    AbstractKuberayWrapper,
+    RayClusterConfig,
+    RayClusterManifest,
+    RayNodeConfig,
+    ScalingGroup,
+)
+from sematic.utils.exceptions import UnsupportedUsageError, UnsupportedVersionError
+
+
+class StandardKuberaySettingsVar(AbstractPluginSettingsVar):
+    """Settings for the Kubray wrapper.
+
+    Attributes
+    ----------
+    RAY_GPU_NODE_SELECTOR:
+        The Kubernetes node selector that will be used for Ray nodes
+        that use GPUs. Value should be json encoded into a string.
+    RAY_NON_GPU_NODE_SELECTOR:
+        The Kubernetes node selector that will be used for Ray nodes
+        that don't use GPUs. Value should be json encoded into a string.
+    RAY_GPU_TOLERATIONS:
+        The Kubernetes tolerations that will be used for Ray nodes
+        that use GPUs. Value should be json encoded into a string.
+    RAY_NON_GPU_TOLERATIONS:
+        The Kubernetes tolerations that will be used for Ray nodes
+        that don't use GPUs. Value should be json encoded into a string.
+    RAY_GPU_RESOURCE_REQUEST_KEY:
+        The key that will be used in the Kubernetes resource requests/
+        limits fields to indicate how many GPUs are required for Ray
+        head/worker nodes. If not specified, requests for more than one
+        GPU will be denied. Value should be json encoded into a string.
+    RAY_SUPPORTS_GPUS:
+        Whether Ray heads/workers can be assigned to Kubernetes nodes with
+        GPUs. Should be either set to "true" or "false".
+    """
+
+    RAY_GPU_NODE_SELECTOR = "RAY_GPU_NODE_SELECTOR"
+    RAY_NON_GPU_NODE_SELECTOR = "RAY_NON_GPU_NODE_SELECTOR"
+    RAY_GPU_TOLERATIONS = "RAY_GPU_TOLERATIONS"
+    RAY_NON_GPU_TOLERATIONS = "RAY_NON_GPU_TOLERATIONS"
+    RAY_GPU_RESOURCE_REQUEST_KEY = "RAY_GPU_RESOURCE_REQUEST_KEY"
+    RAY_SUPPORTS_GPUS = "RAY_SUPPORTS_GPUS"
+
+
+class _NeedsOverride:
+    """Non-encodable signal value to indicate a template value needs to be overridden."""
+
+    pass
+
+
+MIN_SUPPORTED_KUBERAY_VERSION = (0, 4, 0)
+
+
+_WORKER_GROUP_TEMPLATE: Dict[str, Any] = {
+    "replicas": _NeedsOverride,
+    "minReplicas": _NeedsOverride,
+    "maxReplicas": _NeedsOverride,
+    "groupName": _NeedsOverride,
+    "rayStartParams": {"block": "true"},
+    "template": {
+        "spec": {
+            "containers": [
+                {
+                    "name": "ray-worker",
+                    "image": _NeedsOverride,
+                    "lifecycle": {
+                        "preStop": {"exec": {"command": ["/bin/sh", "-c", "ray stop"]}}
+                    },
+                    "volumeMounts": [{"mountPath": "/tmp/ray", "name": "ray-logs"}],
+                    "resources": {
+                        "limits": _NeedsOverride,
+                        "requests": _NeedsOverride,
+                    },
+                }
+            ],
+            "initContainers": [
+                {
+                    "name": "init",
+                    "image": "busybox:1.28",
+                    "command": [
+                        "sh",
+                        "-c",
+                        "until nslookup "
+                        "$RAY_IP."
+                        "$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)"
+                        ".svc.cluster.local; "
+                        "do echo waiting for K8s Service $RAY_IP; sleep 2; done",
+                    ],
+                }
+            ],
+            "tolerations": _NeedsOverride,
+            "nodeSelector": _NeedsOverride,
+            "volumes": [{"name": "ray-logs", "emptyDir": {}}],
+        }
+    },
+}
+
+_MANIFEST_TEMPLATE: Dict[str, Any] = {
+    "apiVersion": "ray.io/v1alpha1",
+    "kind": "RayCluster",
+    "metadata": {
+        "labels": {"controller-tools.k8s.io": "1.0"},
+        "name": _NeedsOverride,
+    },
+    "spec": {
+        "rayVersion": _NeedsOverride,
+        "headGroupSpec": {
+            "serviceType": "ClusterIP",
+            "rayStartParams": {"dashboard-host": "0.0.0.0", "block": "true"},
+            "template": {
+                "metadata": {"labels": {}},
+                "spec": {
+                    "containers": [
+                        {
+                            "name": "ray-head",
+                            "image": _NeedsOverride,
+                            "ports": [
+                                {"containerPort": 6379, "name": "gcs"},
+                                {"containerPort": 8265, "name": "dashboard"},
+                                {"containerPort": 10001, "name": "client"},
+                            ],
+                            "lifecycle": {
+                                "preStop": {
+                                    "exec": {"command": ["/bin/sh", "-c", "ray stop"]}
+                                }
+                            },
+                            "volumeMounts": [
+                                {"mountPath": "/tmp/ray", "name": "ray-logs"}
+                            ],
+                            "resources": {
+                                "limits": _NeedsOverride,
+                                "requests": _NeedsOverride,
+                            },
+                        }
+                    ],
+                    "tolerations": _NeedsOverride,
+                    "nodeSelector": _NeedsOverride,
+                    "volumes": [{"name": "ray-logs", "emptyDir": {}}],
+                },
+            },
+        },
+        "workerGroupSpecs": _NeedsOverride,
+    },
+}
+
+
+class StandardKuberayWrapper(AbstractKuberayWrapper):
+    """Implementation designed for conventional K8s setups and recent Kuberay versions
+
+    This implementation is structured so as to be subclassed to customize specific
+    behaviors if more complex setups are required (e.g. mounting custom volumes to
+    Ray nodes, additional pod lifecycle hooks, resource requests differing from resource
+    limits, etc.).
+
+    It supports GPUs, but only if configured to do so (see StandardKuberaySettingsVar).
+    """
+
+    _manifest_template = _MANIFEST_TEMPLATE
+    _worker_group_template = _WORKER_GROUP_TEMPLATE
+
+    @classmethod
+    def get_settings_vars(cls) -> Type[AbstractPluginSettingsVar]:
+        return StandardKuberaySettingsVar
+
+    @classmethod
+    def create_cluster_manifest(  # type: ignore
+        cls,
+        image_uri: str,
+        cluster_name: str,
+        cluster_config: RayClusterConfig,
+        kuberay_version: str,
+    ) -> RayClusterManifest:
+        cls._validate_kuberay_version(kuberay_version)
+        cls._validate_cluster_config(cluster_config=cluster_config)
+        manifest = deepcopy(cls._manifest_template)
+        manifest["metadata"]["name"] = cluster_name
+        manifest["spec"]["rayVersion"] = cluster_config.ray_version
+
+        head_group_spec = cls._make_head_group_spec(
+            image_uri, cluster_config.head_node, manifest["spec"]["headGroupSpec"]
+        )
+        manifest["spec"]["headGroupSpec"] = head_group_spec
+        manifest["spec"]["workerGroupSpecs"] = [
+            cls._make_worker_group_spec(image_uri, group, i)
+            for i, group in enumerate(cluster_config.scaling_groups)
+        ]
+
+        return manifest
+
+    @classmethod
+    def _make_worker_group_spec(
+        cls, image_uri: str, worker_group: ScalingGroup, group_index: int
+    ):
+        group_manifest = deepcopy(cls._worker_group_template)
+        group_manifest["replicas"] = worker_group.min_workers
+        group_manifest["minReplicas"] = worker_group.min_workers
+        group_manifest["maxReplicas"] = worker_group.max_workers
+        group_manifest["groupName"] = f"worker-group-{group_index}"
+        group_manifest["template"]["spec"]["containers"][0]["image"] = image_uri
+        group_manifest["template"]["spec"]["containers"][0]["resources"][
+            "limits"
+        ] = cls._limits_for_node(worker_group.worker_nodes)
+        group_manifest["template"]["spec"]["containers"][0]["resources"][
+            "requests"
+        ] = cls._requests_for_node(worker_group.worker_nodes)
+        group_manifest["template"]["spec"]["nodeSelector"] = cls._get_node_selector(
+            worker_group.worker_nodes
+        )
+        group_manifest["template"]["spec"]["tolerations"] = cls._get_tolerations(
+            worker_group.worker_nodes
+        )
+
+        return group_manifest
+
+    @classmethod
+    def _validate_kuberay_version(cls, kuberay_version: str):
+        int_tuple_version = tuple(
+            int(v) for v in kuberay_version.replace("v", "").split(".")
+        )
+        if (
+            len(int_tuple_version) != 3
+            or int_tuple_version < MIN_SUPPORTED_KUBERAY_VERSION
+        ):
+            raise UnsupportedVersionError(
+                f"Unsupported version of Kuberay: {kuberay_version}. "
+                f"Min supported version: {MIN_SUPPORTED_KUBERAY_VERSION}."
+            )
+
+    @classmethod
+    def _validate_cluster_config(cls, cluster_config: RayClusterConfig) -> None:
+        cls._validate_ray_version(cluster_config.ray_version)
+        cls._validate_gpu_config(cluster_config)
+
+    @classmethod
+    def _validate_gpu_config(cls, cluster_config: RayClusterConfig) -> None:
+        requires_gpus = cluster_config.head_node.gpu_count != 0 or any(
+            group.worker_nodes.gpu_count != 0 for group in cluster_config.scaling_groups
+        )
+        supports_gpus = _get_setting(
+            StandardKuberaySettingsVar.RAY_SUPPORTS_GPUS, False
+        )
+        if requires_gpus and not supports_gpus:
+            raise UnsupportedUsageError(
+                f"The Kuberay plugin {cls.__name__} is not configured "
+                "to support nodes with GPUs"
+            )
+
+    @classmethod
+    def _validate_ray_version(cls, ray_version: str) -> None:
+        if not ray_version.replace("v", "").startswith("2"):
+            raise UnsupportedVersionError(
+                "Only ray versions 2.0 or higher are supported."
+            )
+
+    @classmethod
+    def _make_head_group_spec(
+        cls,
+        image_uri: str,
+        node_config: RayNodeConfig,
+        head_group_template: Dict[str, Any],
+    ) -> Dict[str, Any]:
+        head_group_template["template"]["spec"]["containers"][0]["image"] = image_uri
+        head_group_template["template"]["spec"]["containers"][0]["resources"][
+            "limits"
+        ] = cls._limits_for_node(node_config)
+        head_group_template["template"]["spec"]["containers"][0]["resources"][
+            "requests"
+        ] = cls._requests_for_node(node_config)
+        head_group_template["template"]["spec"][
+            "nodeSelector"
+        ] = cls._get_node_selector(node_config)
+        head_group_template["template"]["spec"]["tolerations"] = cls._get_tolerations(
+            node_config
+        )
+
+        return head_group_template
+
+    @classmethod
+    def _get_node_selector(cls, node_config: RayNodeConfig) -> Dict[str, Any]:
+        uses_gpus = node_config.gpu_count > 0
+        setting = (
+            StandardKuberaySettingsVar.RAY_GPU_NODE_SELECTOR
+            if uses_gpus
+            else StandardKuberaySettingsVar.RAY_NON_GPU_NODE_SELECTOR
+        )
+
+        node_selector = _get_setting(setting, {})
+
+        return node_selector
+
+    @classmethod
+    def _get_tolerations(cls, node_config: RayNodeConfig) -> Dict[str, Any]:
+        uses_gpus = node_config.gpu_count > 0
+        setting = (
+            StandardKuberaySettingsVar.RAY_GPU_TOLERATIONS
+            if uses_gpus
+            else StandardKuberaySettingsVar.RAY_NON_GPU_TOLERATIONS
+        )
+        tolerations = _get_setting(setting, [])
+        return tolerations
+
+    @classmethod
+    def _limits_for_node(cls, node_config: RayNodeConfig) -> Dict[str, str]:
+        # The standard plugin always makes requests & limits the same
+        return cls._requests_for_node(node_config)
+
+    @classmethod
+    def _requests_for_node(cls, node_config: RayNodeConfig) -> Dict[str, str]:
+        gpu_requests = {}
+        if node_config.gpu_count > 0:
+            gpu_request_key = _get_setting(
+                StandardKuberaySettingsVar.RAY_GPU_RESOURCE_REQUEST_KEY, None
+            )
+            if gpu_request_key is None:
+                if node_config.gpu_count > 1:
+                    raise UnsupportedUsageError(
+                        "You are requesting more than one GPU per node, but the server "
+                        "is not configured to support more than one GPU per node."
+                    )
+            else:
+                gpu_requests[gpu_request_key] = node_config.gpu_count
+
+        milli_cpu = int(1000 * node_config.cpu)
+        memory_mb = int(1024 * node_config.memory_gb)
+        requests = {
+            "cpu": f"{milli_cpu}m",
+            "memory": f"{memory_mb}M",
+        }
+        requests.update(gpu_requests)
+        return requests
+
+
+def _get_setting(setting, default):
+    return json.loads(
+        get_plugin_setting(StandardKuberayWrapper, setting, json.dumps(default))
+    )
