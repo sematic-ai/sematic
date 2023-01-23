e22e3d271e5ee9f0aa52fd75fdc077a0fcae42bd
augray <apps@joshbauer.email>
2023-01-23 09:30:22 -0800

    proper dep

--- sematic/examples/testing_pipeline/pipeline.py
+++ sematic/examples/testing_pipeline/pipeline.py
@@ -13,8 +13,8 @@ import ray
 
 # Sematic
 import sematic
-from sematic.plugins.abstract_external_resource.ray.cluster import RayCluster
 from sematic.plugins.abstract_kuberay_wrapper import RayNodeConfig, SimpleRayCluster
+from sematic.plugins.external_resource.ray.cluster import RayCluster
 from sematic.plugins.external_resource.timed_message import TimedMessage
 from sematic.resolvers.resource_requirements import ResourceRequirements
 
