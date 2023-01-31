a594848191eaa647fccd71e614ae1b503833191a
Emmanuel Turlay <emmanuel@sematic.ai>
2023-01-30 18:08:14 -0800

    Fix tests

--- sematic/api/endpoints/tests/test_uploads.py
+++ sematic/api/endpoints/tests/test_uploads.py
@@ -1,6 +1,3 @@
-# Standard Library
-from unittest import mock
-
 # Third-party
 import flask
 import flask.testing
@@ -8,14 +5,9 @@ import flask.testing
 # Sematic
 from sematic.api.tests.fixtures import mock_auth, test_client  # noqa: F401
 from sematic.db.tests.fixtures import test_db  # noqa: F401
-from sematic.plugins.storage.memory_storage import MemoryStorage
 
 
-@mock.patch(
-    "sematic.api.endpoints.uploads._get_storage_plugin", return_value=MemoryStorage
-)
 def test_upload_download(
-    mock_plugin,
     mock_auth,  # noqa: F811
     test_client: flask.testing.FlaskClient,  # noqa: F811
 ):
@@ -24,9 +16,9 @@ def test_upload_download(
     response = test_client.get("/api/v1/uploads/artifacts/123/location")
     assert response.status_code == 200
 
-    location = response.json["location"]  # type: ignore
+    url = response.json["url"]  # type: ignore
 
-    response = test_client.put(location, data=value)
+    response = test_client.put(url, data=value)
     assert response.status_code == 200
 
     response = test_client.get("/api/v1/uploads/artifacts/123/data")
