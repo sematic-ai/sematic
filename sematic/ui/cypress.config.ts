f275d00a6d5ef5ebc60309bcefe4793dd2b30f2b
Chance An <anchangsi@gmail.com>
2023-02-01 17:03:41 -0800

    Introduces cypress test

---
+++ sematic/ui/cypress.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+  videosFolder: './cypress_video'
+});
