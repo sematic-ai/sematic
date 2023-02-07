c03c36532ed0a3eb3b396bc059893df536ae6998
augray <apps@joshbauer.email>
2023-02-07 07:55:38 -0800

    Add JobStatus type

--- sematic/scheduling/external_job.py
+++ sematic/scheduling/external_job.py
@@ -1,6 +1,7 @@
 # Standard Library
 import enum
-from dataclasses import dataclass
+import time
+from dataclasses import dataclass, field
 from typing import Optional
 
 # Sematic
@@ -12,6 +13,30 @@ KUBERNETES_JOB_KIND = "k8s"
 class JobType(enum.Enum):
     driver = "driver"
     worker = "worker"
+
+
+@dataclass
+class JobStatus:
+    """A simple status object describing the state of the job
+
+    Attributes
+    ----------
+    state_name:
+        This should be a one-word descriptor of the state of the job. Ex:
+        "Pending", "Running", etc.
+    description:
+        This should be a human-readable description of the state of the job,
+        no more than a couple short sentences pointing out any unique details
+        about the state of the job.
+    last_update_epoch_time:
+        The time this status object was generated, as epoch seconds.
+    """
+
+    state_name: str
+    description: str
+    last_update_epoch_time: int = field(
+        default_factory=lambda: int(time.time()), compare=False
+    )
 
 
 @dataclass
@@ -40,7 +65,7 @@ class ExternalJob:
 
         Active in this context means whether or not it may still evolve
         the run's future. Note that this method is based only on the
-        in-memory RunJob and doesn't reach out to the external job source.
+        in-memory ExternalJob and doesn't reach out to the external job source.
 
         Returns
         -------
@@ -53,3 +78,16 @@ class ExternalJob:
         failure.
         """
         return None
+
+    def get_status(self) -> JobStatus:
+        """Get a simple status describing the state of the job.
+
+        Note that the returned status should be based on the in-memory
+        fields of the ExternalJob, and should not reach out to the external
+        job source.
+
+        Returns
+        -------
+        A job status.
+        """
+        raise NotImplementedError("Subclasses of ExternalJob should define get_status")
