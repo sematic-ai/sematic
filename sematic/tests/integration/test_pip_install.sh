858062ebda6230f9a4dc4888c5742ab74f72914a
augray <augray@users.noreply.github.com>
2023-02-09 12:02:40 -0500

    Make wheel slimmer (#552)
    A regression in our wheel building macro patch made it so we were
    stuffing a bunch of irrelevant third party dep files into the wheel.
    This resolves the regression, and improves our basic sanity testing of
    the wheel to include wheel size.

--- sematic/tests/integration/test_pip_install.sh
+++ sematic/tests/integration/test_pip_install.sh
@@ -16,7 +16,27 @@ pwd
 
 source ./$VENV_NAME/bin/activate
 
+WHEEL_PATH=$(ls bazel-bin/sematic/sematic-*.whl)
+
+if test -f "$WHEEL_PATH"; then
+    echo "Wheel found at $WHEEL_PATH"
+else
+    echo "Wheel not present at $WHEEL_PATH"
+    exit 1
+fi
+
+N_MB_SIZE_LIMIT=10
+WHEEL_SIZE_MB=$(python3 -c "import os; print(int(os.path.getsize('$WHEEL_PATH') / 2**20))")
+
+if (( WHEEL_SIZE_BYTES > N_BYTE_SIZE_LIMIT )); then
+    echo "Error: Wheel bigger than $N_MB_SIZE_LIMIT Mb. Size: $WHEEL_SIZE_MB Mb"
+    exit 1
+else
+    echo "Wheel is $WHEEL_SIZE_MB Mb"
+fi
+
 pip install bazel-bin/sematic/sematic-*.whl
+python3 -c "import sematic; print(sematic.__version__)" || exit 1
 
 deactivate
 
