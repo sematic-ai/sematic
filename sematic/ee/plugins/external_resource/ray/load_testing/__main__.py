455ff7ce578349262041a21b517615442ab3cd1f
Josh Bauer <josh@sematic.dev>
2023-02-10 21:50:58 +0000

    WIP

---
+++ sematic/ee/plugins/external_resource/ray/load_testing/__main__.py
@@ -0,0 +1,142 @@
+"""
+Entry point for the testing pipeline.
+"""
+# Standard Library
+import argparse
+import logging
+import os
+
+# Third-party
+import debugpy
+
+# Sematic
+from sematic import CloudResolver, LocalResolver, Resolver, SilentResolver
+from sematic.ee.plugins.external_resource.ray.load_testing.pipeline import (
+    CollatzConfig,
+    load_test_ray,
+)
+
+logger = logging.getLogger(__name__)
+
+BAZEL_COMMAND = (
+    "bazel run //sematic/ee/plugins/external_resource/ray/load_testing:__main__ --"
+)
+DESCRIPTION = (
+    "This pipeline is used to perform load testing on the Ray integration. "
+    "The arguments control the shape of the pipeline, as described in the individual "
+    "help strings. "
+)
+CLOUD_HELP = (
+    "Whether to run the resolution in the cloud, or locally. Defaults to False. "
+    "Only one of --silent or --cloud are allowed."
+)
+SILENT_HELP = (
+    "Whether to run the resolution using the SilentResolver. Defaults to False. "
+    "Only one of --silent or --cloud are allowed."
+)
+COLLATZ_N_WORKERS_HELP = ""
+COLLATZ_N_TASKS_HELP = ""
+COLLATZ_WAIT_MINUTES_HELP = ""
+COLLATZ_MEMORY_GROWTH_FACTOR_HELP = ""
+
+
+def _parse_args() -> argparse.Namespace:
+    """Parses the command line arguments."""
+    parser = argparse.ArgumentParser(prog=BAZEL_COMMAND, description=DESCRIPTION)
+
+    # Resolver args:
+    parser.add_argument(
+        "--cloud",
+        action="store_true",
+        default=False,
+        help=CLOUD_HELP,
+    )
+    parser.add_argument(
+        "--silent",
+        action="store_true",
+        default=False,
+        help=SILENT_HELP,
+    )
+    parser.add_argument(
+        "--collatz.n_workers",
+        type=int,
+        default=0,
+        dest="collatz_n_workers",
+        help=COLLATZ_N_WORKERS_HELP,
+    )
+    parser.add_argument(
+        "--collatz.n_tasks",
+        type=int,
+        default=10,
+        dest="collatz_n_tasks",
+        help=COLLATZ_N_TASKS_HELP,
+    )
+    parser.add_argument(
+        "--collatz.wait_minutes",
+        type=int,
+        default=1,
+        dest="collatz_wait_minutes",
+        help=COLLATZ_WAIT_MINUTES_HELP,
+    )
+    parser.add_argument(
+        "--collatz.memory_growth_factor",
+        type=float,
+        default=1.10,
+        dest="collatz_memory_growth_factor",
+        help=COLLATZ_MEMORY_GROWTH_FACTOR_HELP,
+    )
+
+    args = parser.parse_args()
+
+    return args
+
+
+def _get_resolver(args: argparse.Namespace) -> Resolver:
+    """Instantiates the Resolver based on the passed arguments."""
+    if args.silent:
+        return SilentResolver()
+    if not args.cloud:
+        return LocalResolver()
+
+    return CloudResolver(
+        detach=True,
+    )
+
+
+def _wait_for_debugger():
+    debugpy.listen(5724)
+
+    print("Waiting for debugger to attach...")
+    debugpy.wait_for_client()
+    print("Debugger attached")
+
+
+def main() -> None:
+    if os.environ.get("DEBUGPY", None) is not None:
+        _wait_for_debugger()
+
+    args = _parse_args()
+    logger.info("Command line arguments: %s", args)
+
+    collatz_config_args = {
+        arg_key.replace("collatz_", ""): arg_val
+        for arg_key, arg_val in vars(args).items()
+        if arg_key.startswith("collatz_")
+    }
+    collatz_config = CollatzConfig(**collatz_config_args)
+    if collatz_config.n_workers == 0:
+        collatz_config = None
+
+    resolver = _get_resolver(args)
+
+    future = load_test_ray(collatz_config).set(
+        tags=["load-testing", "ray"],
+    )
+
+    logger.info("Invoking the pipeline...")
+    result = future.resolve(resolver)
+    logger.info("Pipeline result: %s", result)
+
+
+if __name__ == "__main__":
+    main()
---
+++ sematic/ee/plugins/external_resource/ray/load_testing/pipeline.py
@@ -0,0 +1,235 @@
+"""
+Flexible-structure pipeline meant to be used in testing.
+"""
+# Standard Library
+import logging
+import time
+from dataclasses import asdict, dataclass
+from typing import Dict, List, Optional, Tuple
+
+# Third-party
+import ray
+import torch
+import torch.nn as nn
+from torch.optim import Adadelta
+from torch.optim.lr_scheduler import StepLR
+from torch.utils.data import DataLoader
+from torchvision.datasets import MNIST
+from torchvision.transforms import Compose, Normalize, ToTensor
+
+# Sematic
+from sematic.calculator import func
+from sematic.ee.ray import RayCluster, RayNodeConfig, SimpleRayCluster
+from sematic.examples.mnist.pytorch.train_eval import Net, train
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class CollatzResults:
+    n_ready: int
+    n_not_ready: int
+    fraction_ready: float
+    results: Dict[int, int]
+
+
+@dataclass
+class MnistResults:
+    learning_rate_to_loss: List[Tuple[float, float]]
+
+
+@dataclass
+class CollatzConfig:
+    n_workers: int
+    n_tasks: int
+    wait_minutes: int
+    memory_growth_factor: float
+
+
+@dataclass
+class LoadResults:
+    collatz_results: CollatzResults
+    mnist_results: MnistResults
+
+
+@func(inline=False)
+def collatz_with_ray(
+    n_workers: int, n_tasks: int, wait_minutes: int, memory_growth_factor: float
+) -> CollatzResults:
+    """
+    Adds two numbers, using a Ray cluster.
+    """
+    max_wait_seconds = wait_minutes * 60
+    logger.info("Determining max Collatz length for numbers less than %s", n_tasks)
+    with RayCluster(
+        config=SimpleRayCluster(
+            n_nodes=n_workers, node_config=RayNodeConfig(cpu=1, memory_gb=2)
+        )
+    ):
+        refs = [
+            collatz_ray_task.remote(n, memory_growth_factor) for n in range(0, n_tasks)
+        ]
+        results = wait_for_results(refs, range(0, n_tasks), max_wait_seconds)
+    return CollatzResults(
+        n_ready=len(results),
+        n_not_ready=len(refs) - len(results),
+        fraction_ready=len(results) / len(refs),
+        results={k: v for k, v in results},
+    )
+
+
+@ray.remote
+def collatz_ray_task(n: int, memory_growth_factor: float):
+    # create new logger due to this:
+    # https://stackoverflow.com/a/55286452/2540669
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    n_iterations = 0
+    intentional_memory_bomb = "x"
+    original_n = n
+    logger.info("Working on Collatz for %s", original_n)
+    while n > 1:
+        intentional_memory_bomb = (
+            int(memory_growth_factor * len(intentional_memory_bomb)) + 1
+        ) * "x"
+        n_iterations += 1
+        time.sleep(0.01)
+        n_mbs = int(len(intentional_memory_bomb) / 2**20)
+        if n_mbs > 50:
+            logger.info(
+                "While working on %s, Bomb size: %s MB",
+                original_n,
+                n_mbs,
+            )
+        if n % 2 == 0:
+            n = n / 2
+        else:
+            n = 3 * n + 1
+    logger.info("Collatz length of %s: %s", original_n, n_iterations)
+    return n_iterations
+
+
+def wait_for_results(refs, inputs, max_wait_seconds):
+    if len(refs) != len(inputs):
+        raise ValueError("Object ref count and input count must match")
+    start_time = time.time()
+    n_ready = 0
+    n_total = len(refs)
+    n_not_ready = len(refs)
+    results_by_ref = {}
+    inputs_by_ref = {ref.hex(): input_val for ref, input_val in zip(refs, inputs)}
+    while n_not_ready != 0 and time.time() - start_time < max_wait_seconds:
+        for ref in refs:
+            if ref.hex() not in results_by_ref:
+                try:
+                    result = ray.get(ref, timeout=0)
+                    results_by_ref[ref.hex()] = result
+                except Exception:
+                    pass
+        time.sleep(10)
+        n_ready = len(results_by_ref)
+        n_not_ready = n_total - n_ready
+        logger.info(
+            "Progress: %s ready, %s not ready out of %s", n_ready, n_not_ready, n_total
+        )
+    return [
+        (inputs_by_ref[ref.hex()], results_by_ref[ref.hex()])
+        for ref in refs
+        if ref in results_by_ref
+    ]
+
+
+@func(inline=False)
+def load_test_mnist() -> MnistResults:
+    n_rates = 10
+    n_epochs = 1
+    learning_rates = [i / n_rates for i in range(0, n_rates)]
+    n_workers = 2
+    max_wait_seconds = 10 * 60
+    with RayCluster(
+        config=SimpleRayCluster(
+            n_nodes=n_workers,
+            node_config=RayNodeConfig(cpu=4, memory_gb=8, gpu_count=1),
+        )
+    ):
+        refs = [train_model.remote(rate, n_epochs) for rate in learning_rates]
+        results = wait_for_results(refs, learning_rates, max_wait_seconds)
+    results = [(k, 0.1) for k, _ in results]
+    return MnistResults(learning_rate_to_loss=results)
+
+
+@ray.remote(num_cpus=1, num_gpus=1, memory=int(1.5 * 2**30))
+def train_model(
+    learning_rate: float,
+    n_epochs: int,
+) -> nn.Module:
+    """Train the model"""
+    device = torch.device("cuda")
+    path = "/tmp/pytorch-mnist"
+    model = Net().to(device)
+    optimizer = Adadelta(model.parameters(), lr=learning_rate)
+    scheduler = StepLR(optimizer, step_size=1, gamma=0.7)
+    log_interval = 10
+    batch_size = 1000
+
+    transform = Compose([ToTensor(), Normalize((0.1307,), (0.3081,))])
+    mnist_dataset = MNIST(root=path, train=True, download=True, transform=transform)
+    train_loader = DataLoader(mnist_dataset, batch_size=batch_size)
+
+    for epoch in range(1, n_epochs + 1):
+        train(
+            model,
+            device,
+            train_loader,
+            optimizer,
+            epoch,
+            log_interval,
+            dry_run=False
+        )
+        scheduler.step()
+
+    return model
+
+
+@func(inline=True)
+def make_results(
+    collatz_results: CollatzResults, mnist_results: MnistResults
+) -> LoadResults:
+    return LoadResults(
+        collatz_results=collatz_results,
+        mnist_results=mnist_results,
+    )
+
+
+@func(inline=True)
+def load_test_ray(
+    collatz_config: Optional[CollatzConfig] = None,
+) -> LoadResults:
+    """
+    The root function of the testing pipeline.
+
+    Its parameters control the actual shape of the pipeline, according to testing needs.
+
+    Parameters
+    ----------
+    collatz_config:
+        How to perform Collatz load testing (a variety of fairly fast tasks
+        with a broad range of lengths and memory requirements).
+    """
+    collatz_results = CollatzResults(
+        n_ready=0,
+        n_not_ready=0,
+        fraction_ready=0,
+        results={},
+    )
+
+    if collatz_config is not None:
+        collatz_results = collatz_with_ray(**asdict(collatz_config))
+
+    mnist_results = MnistResults(
+        learning_rate_to_loss=[],
+    )
+    mnist_results = load_test_mnist()
+
+    return make_results(collatz_results, mnist_results)
