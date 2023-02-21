51015bca5376cbd960da7f3aa523d14b7b62f6e2
Emmanuel Turlay <emmanuel@sematic.ai>
2023-02-21 01:11:02 +0000

    Generating IDs in Python for SQLite/PostgreSQL compat

--- sematic/db/migrations/20230217235835_add_id_to_users.sql
+++ sematic/db/migrations/20230217235835_add_id_to_users.sql
@@ -13,42 +13,16 @@ CREATE TABLE users_tmp (
     PRIMARY KEY (id)
 );
 
-INSERT INTO users_tmp (
-    id, email, first_name, last_name, avatar_url, api_key, created_at, updated_at
-) SELECT 
-    lower(hex(randomblob(16))), email, first_name, last_name, avatar_url,
-    api_key, created_at, updated_at
-FROM users;
 
-ALTER TABLE users RENAME TO users_backup;
+-- INSERT INTO users_tmp (
+--    id, email, first_name, last_name, avatar_url, api_key, created_at, updated_at
+-- ) SELECT 
+--    lower(hex(randomblob(16))), email, first_name, last_name, avatar_url,
+--    api_key, created_at, updated_at
+-- FROM users;
 
-ALTER TABLE users_tmp RENAME TO users;
 
-DROP TABLE users_backup;
 
 -- migrate:down
 
-CREATE TABLE users_tmp (
-    email TEXT NOT NULL,
-    first_name TEXT,
-    last_name TEXT,
-    avatar_url TEXT,
-    api_key TEXT NOT NULL,
-    created_at timestamp NOT NULL,
-    updated_at timestamp NOT NULL,
-
-    PRIMARY KEY (email)
-);
-
-INSERT INTO users_tmp (
-    email, first_name, last_name, avatar_url, api_key, created_at, updated_at
-) SELECT 
-    email, first_name, last_name, avatar_url,
-    api_key, created_at, updated_at
-FROM users;
-
-ALTER TABLE users RENAME TO users_backup;
-
-ALTER TABLE users_tmp RENAME TO users;
-
-DROP TABLE users_backup;
+DROP TABLE users_tmp;
