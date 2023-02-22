be7cd2f9136592647528d0d8fd39ef5d1b0defd6
Sash Nagarkar <sashn45@gmail.com>
2023-02-22 11:48:10 -0800

    Atomic database migrations with Helm hooks (#589)
    Resolve #588.
    
    * Runs the migration as a separate container before starting api/socket
    servers.
    * Does not start the servers if migration failed.
    * Keeps the migration pod around in a Completed state until the next
    deploy so logs are accessible.
    * Configured a logger explicitly since the CLI call doesn't.
    * Cleaned up the Helm install notes a bit.

--- sematic/cli/main.py
+++ sematic/cli/main.py
@@ -1,11 +1,4 @@
 # Sematic
-from sematic.config.config import switch_env
-from sematic.db.migrate import migrate_up
-
-switch_env("local")
-migrate_up()
-
-# Sematic
 import sematic.cli.cancel  # noqa: F401, E402
 import sematic.cli.logs  # noqa: F401, E402
 import sematic.cli.migrate  # noqa: F401, E402
