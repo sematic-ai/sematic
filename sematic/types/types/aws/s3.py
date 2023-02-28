3b4aa668d7403cae84528e6c7a865fb311f08d97
Emmanuel Turlay <emmanuel@sematic.ai>
2023-02-28 15:52:45 -0800

    S3Location and S3Bucket types and viz

--- sematic/types/types/aws/s3.py
+++ sematic/types/types/aws/s3.py
@@ -4,11 +4,19 @@ from dataclasses import dataclass
 
 @dataclass
 class S3Bucket:
+    """
+    Basic type to describe an S3 bucket.
+    """
+
     region: str
     name: str
 
 
 @dataclass
 class S3Location:
+    """
+    Basic type to describe an S3 location.
+    """
+
     bucket: S3Bucket
     location: str
