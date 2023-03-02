a181f27ede7a5ea5b6614a26531ccb9ea9384030
Emmanuel Turlay <emmanuel@sematic.ai>
2023-03-01 17:49:14 -0800

    Adding APIs to S3Location

--- sematic/types/types/aws/s3.py
+++ sematic/types/types/aws/s3.py
@@ -55,3 +55,51 @@ class S3Location:
         return S3Location(
             bucket=S3Bucket(name=split_uri.netloc, region=region), location=location
         )
+
+    @property
+    def parent_directory(self) -> "S3Location":
+        """
+        The S3Location of the parent directory.
+
+        If current location is a file, the parent directory is the directory in
+        which the file is stored. If the current location is a directory, the
+        parent directory is its parent directory.
+
+        Returns
+        -------
+        S3Location
+        """
+        location = self.location[:-1] if self.location[-1] == "/" else self.location
+        location_parts = location.split("/")
+
+        return S3Location(bucket=self.bucket, location="/".join(location_parts[:-1]))
+
+    def sibling_location(self, location: str) -> "S3Location":
+        """
+        A location in the same parent directory as the current location.
+
+        Returns
+        -------
+        S3Location
+        """
+        return S3Location(
+            bucket=self.bucket,
+            location="/".join([self.parent_directory.location, location]),
+        )
+
+    def child_location(self, location: str) -> "S3Location":
+        """
+        A location under the current location.
+
+        Returns
+        -------
+        S3Location
+        """
+        child_location = self.location
+        if not child_location.endswith("/"):
+            child_location = child_location + "/"
+
+        return S3Location(bucket=self.bucket, location=f"{child_location}{location}")
+
+    def __truediv__(self, location: str) -> "S3Location":
+        return self.child_location(location)
