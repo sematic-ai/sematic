9cf5d7f1a19e1913683847c07407854aacf2bb59
Emmanuel Turlay <emmanuel@sematic.ai>
2023-03-08 18:04:48 +0000

    Fix determinism of dict serialization

--- sematic/types/types/dict.py
+++ sematic/types/types/dict.py
@@ -1,4 +1,6 @@
 # Standard Library
+import hashlib
+import json
 from typing import Any, Dict, List, Literal, Mapping, Optional, Tuple, Type, get_args
 
 # Sematic
@@ -15,6 +17,7 @@ from sematic.types.serialization import (
     value_from_json_encodable,
     value_to_json_encodable,
 )
+from sematic.utils.hashing import get_str_sha1_digest
 
 
 @register_safe_cast(dict)
@@ -106,23 +109,23 @@ def _dict_to_json_encodable_summary(value: Dict, type_: Type) -> SummaryOutput:
     """
     key_type, element_type = type_.__args__
 
-    # Sorting keys for determinism
-    # not using the values of the keys directly because the '<' operation is not
-    # guaranteed to be implemented for all types, but the hash is guaranteed to be
-    # implemented, since the keys must be hashable in order to be used as keys
-    sorted_keys = sorted(value.keys(), key=hash)
+    blobs = {}
+    unsorted_summaries = {}
 
-    summary, blobs = [], {}
-
-    for key in sorted_keys:
+    for key, value_ in value.items():
         key_summary, key_blobs = get_json_encodable_summary(key, key_type)
+        value_summary, value_blobs = get_json_encodable_summary(value_, element_type)
 
-        value_summary, value_blobs = get_json_encodable_summary(
-            value[key], element_type
-        )
-
-        summary.append((key_summary, value_summary))
         blobs.update(value_blobs)
         blobs.update(key_blobs)
+
+        # Sorting keys for determinism
+        # not using the values of the keys directly because the '<' operation is not
+        # guaranteed to be implemented for all types.
+        # Using sha1 of serialization
+        hashed_key = get_str_sha1_digest(json.dumps(key_summary, sort_keys=True))
+        unsorted_summaries[hashed_key] = (key_summary, value_summary)
+
+    summary = [unsorted_summaries[key] for key in sorted(unsorted_summaries.keys())]
 
     return summary, blobs
