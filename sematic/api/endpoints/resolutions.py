31c9b005ca5dd1635c560c6d4aa82fb9aa0f17ff
augray <apps@joshbauer.email>
2023-03-13 06:51:52 -0700

    Add more info to terminal status log

--- sematic/api/endpoints/resolutions.py
+++ sematic/api/endpoints/resolutions.py
@@ -6,6 +6,7 @@ Module keeping all /api/v*/runs/* API endpoints.
 import logging
 from http import HTTPStatus
 from typing import Optional
+from datetime import datetime
 
 # Third-party
 import flask
@@ -146,13 +147,19 @@ def put_resolution_endpoint(user: Optional[User], resolution_id: str) -> flask.R
             # status for usage in dashboards. Some users may be leveraging it for
             # such purposes, so think carefully before changing/removing it.
             was_remote = len(resolution.external_jobs)
+            duration_seconds = None
+            if root_run.started_at is not None:
+                duration_seconds = (datetime.utcnow() - root_run.started_at).total_seconds()
             logger.info(
                 "%s resolution %s for pipeline %s terminated with "
-                "root run in state %s. The root run had tags: %s",
+                "root run in state %s. The duration was %s seconds. Git dirty: %s ."
+                "The root run had tags: %s",
                 "Remote" if was_remote else "Local",
                 resolution.root_id,
                 root_run.calculator_path,
                 root_run.future_state,
+                duration_seconds,
+                resolution.git_info.dirty,
                 root_run.tags,
             )
         try:
