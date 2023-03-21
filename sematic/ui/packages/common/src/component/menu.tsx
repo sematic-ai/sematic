2054d8701a5d4d55dfba72ebadb04456614a7c69
Chance An <anchangsi@gmail.com>
2023-03-21 13:08:47 -0700

    Implement the components and styles for the new header bar (#683)
    Implement the components and styles for the new header bar
    
    Set default component styles for MUI Theme and created button variants
    for the Logo and MenuItem styles.
    
    Used the SVG component for the branch logo image. 
    
    To preview the current storybook:
    https://story.dev-usw2-sematic0.sematic.cloud
    
    
    
    ![capture1](https://user-images.githubusercontent.com/1046489/226510002-bf70360c-d7cb-49a4-a474-171ed2260417.gif)

---
+++ sematic/ui/packages/common/src/component/menu.tsx
@@ -0,0 +1,41 @@
+import styled from "@emotion/styled";
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import useTheme from "@mui/material/styles/useTheme";
+import Fox from "src/static/fox";
+import palette from "src/theme/new/palette";
+import { SimplePaletteColorOptions } from "@mui/material/styles";
+
+const StyledGridContainer = styled(Grid)`
+    border-bottom: 1px solid ${() => (palette.p3border as SimplePaletteColorOptions).main};
+`;
+
+interface HeaderMenuProps {
+    selectedKey?: string;
+}
+
+const HeaderMenu = (props: HeaderMenuProps) => {
+    const {selectedKey} = props;
+    const theme = useTheme();
+
+    return <StyledGridContainer container spacing={0}>
+        <Box style={{flexGrow: 1, display: 'flex'}} >
+            <Link variant={'logo'} style={{marginRight: theme.spacing(6)}}>
+                <Fox style={{width: '16px'}}/>
+            </Link>
+            
+            <Link variant="subtitle1" type='menu' className={selectedKey === 'runs'? 'selected' : ''}>Runs</Link>
+            <Link variant="subtitle1" type='menu' className={selectedKey === 'pipelines'? 'selected' : ''}>Pipelines</Link>
+            <Link variant="subtitle1" type='menu' className={selectedKey === 'metrics'? 'selected' : ''}>Metrics</Link>
+        </Box>
+        <Box style={{flexGrow: 1, display: 'flex', justifyContent: 'end'}} >
+            <Link variant="subtitle1" type='menu'>Get Started</Link>
+            <Link variant="subtitle1" type='menu'>Docs</Link>
+            <Link variant="subtitle1" type='menu'>Support</Link>
+            <Link variant="subtitle1" type='menu'>Developer E</Link>
+        </Box>
+    </StyledGridContainer>;
+};
+
+export default HeaderMenu;
--- sematic/ui/packages/common/src/theme/new/palette.ts
+++ sematic/ui/packages/common/src/theme/new/palette.ts
@@ -1,22 +1,9 @@
-import { blue, lightBlue, red, green, common } from "@mui/material/colors";
+import { blue, common } from "@mui/material/colors";
 import { PaletteColorOptions } from "@mui/material/styles";
 
 const pallette: Record<string, PaletteColorOptions> = {
     primary: {
         main: blue[500]
-    },
-    blue: {
-        main: lightBlue[500]
-    },
-    error: {
-        main: red[700]
-    },
-    warning: {
-        main: '#ed6c02'
-    },
-    success: {
-        main: green[800],
-        contrastText: '#eaf2ea'
     },
     lightGrey: {
         main: '#f5f5f5'
