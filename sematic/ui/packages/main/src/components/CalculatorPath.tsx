484fa1972c06598966a471d3136a4eabd9f7655a
Chance An <anchangsi@gmail.com>
2023-03-21 13:07:10 -0700

    Fixes a UI bug that the content of a table cell would explode the table width and disrupt column alignment (#689)
    Before:
    
    
    ![capture1](https://user-images.githubusercontent.com/1046489/226705428-3b70ce6d-2e0f-4838-b96e-ee2db2b14531.gif)
    
    After:
    
    <img width="1179" alt="image"
    src="https://user-images.githubusercontent.com/1046489/226704968-7fc3751a-3463-47b2-8f0b-fd32767bc9c6.png">

--- sematic/ui/packages/main/src/components/CalculatorPath.tsx
+++ sematic/ui/packages/main/src/components/CalculatorPath.tsx
@@ -3,7 +3,7 @@ import Typography from "@mui/material/Typography";
 function CalculatorPath(props: { calculatorPath: string; component?: string }) {
   return (
     <Typography fontSize="small" color="GrayText" component="span">
-      <code style={{ fontSize: 12 }}>{props.calculatorPath}</code>
+      <code style={{ fontSize: 12, wordWrap: 'break-word' }}>{props.calculatorPath}</code>
     </Typography>
   );
 }
