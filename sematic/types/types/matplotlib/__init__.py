f27be1e31e2e7a2e740ef54c126fc7dda4c8d6c5
Emmanuel Turlay <emmanuel@sematic.ai>
2023-03-28 14:46:49 -0700

    Use  to support

--- sematic/types/types/matplotlib/__init__.py
+++ sematic/types/types/matplotlib/__init__.py
@@ -2,7 +2,9 @@ try:
     # Third-party
     import matplotlib  # noqa: F401
 except ImportError:
+    print("NO MATPLOTLIB")
     pass
 else:
+    print("YES MATPLOTLIB")
     # Sematic
     import sematic.types.types.matplotlib.figure  # noqa: F401
