bbddb1d033f0fda2e886005edb3c811f7e9be4d6
Chance An <anchangsi@gmail.com>
2023-03-31 09:32:43 -0700

    Show cases some of the new components (#702)
    1. Ensure that the following new components of `GitInfoBoxProps`,
    `RunTree`, `RunsDropdown` and `TagsList` can handle different input
    properties and support basic interactivity.
    
    2. Add the complete types for the `RunStateChip` set. 
    
    3. Create new storybook stories for the components mentioned above. 
    
    The change is deployed to 
    
    
    https://story.dev-usw2-sematic0.sematic.cloud/?path=/story/sematic-gitinfo--git-info&args=has+uncommited+changes:!false
    
    for preview.
    
    
    
    ![capture1](https://user-images.githubusercontent.com/1046489/228688339-eb7bb01c-058c-4401-8f01-5e611b3ee4fb.gif)

--- sematic/ui/packages/common/src/context/SnackBarProvider.tsx
+++ sematic/ui/packages/common/src/context/SnackBarProvider.tsx
@@ -3,15 +3,16 @@ import { useCallback } from "react";
 
 interface SnackBarProviderProps {
     children: React.ReactNode;
+    setSnackMessageOverride?: (message: string) => void;
 }
 
 /**
  * This is a temporary solution to show snack messages. It will be replaced with a proper snack bar component.
  */
-const SnackBarProvider = ({ children }: SnackBarProviderProps) => {
+const SnackBarProvider = ({ children, setSnackMessageOverride }: SnackBarProviderProps) => {
     const setSnackMessage = useCallback((message: string) => {
-        console.log(message);
-    }, [])
+        (setSnackMessageOverride || console.log)(message);
+    }, [setSnackMessageOverride])
 
     return <SnackBarContext.Provider value={{ setSnackMessage }}>
         {children}
