4f41d017179a5c9fb2056ac7ca53d11d45239864
Chance An <anchangsi@gmail.com>
2023-04-05 09:27:13 -0700

    Use react hooks in story components (#712)
    Back in [PR 702](https://github.com/sematic-ai/sematic/pull/702) I
    introduced stories for the RunTree component. There I used a class
    component for the story because I had trouble using functional
    components and hooks.
    
    Later investigation indicates that we saw the issue with hooks because,
    in Storybook's setup, we had two copies of React (despite the same
    version). The official React manual
    [here](https://legacy.reactjs.org/warnings/invalid-hook-call-warning.html#duplicate-react)
    explains that.
    
    My solution is to reimport the same react hooks used in the `common`
    package and consume them in the storybook package. This approach is
    easier to manipulate than symbolic linking the react dependency from the
    storybook, which will also have the bundler (webpack) treat the two
    references to the react npm module from `common` and `storybook`
    packages the same.
    
    This PR also converted the class component into the functional component
    in the storybook for the sake of being streamlined.

---
+++ sematic/ui/packages/common/src/reactHooks.ts
@@ -0,0 +1,3 @@
+// These exports are meant to be used by the `storybook` packages, so that they
+// can use the same version of React as this package.
+export { useState, useCallback, useEffect, useRef } from "react";
