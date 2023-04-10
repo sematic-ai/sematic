4fb54e5ad038aaaf6cc89d3a6bb8731efa971a8c
Chance An <anchangsi@gmail.com>
2023-04-10 13:56:58 -0700

    Enable text selection on S3 buttons (#724)
    Before:
    
    
    ![capture1](https://user-images.githubusercontent.com/1046489/230689945-ed162d4f-7abd-4039-ab02-ff94c95f01a2.gif)
    
    After:
    
    
    ![capture1](https://user-images.githubusercontent.com/1046489/230689671-62d6ec31-12f9-4fde-b8b0-bdd023ecaae4.gif)

--- sematic/ui/packages/main/src/types/aws.tsx
+++ sematic/ui/packages/main/src/types/aws.tsx
@@ -2,6 +2,7 @@ import { OpenInNew } from "@mui/icons-material";
 import { Button, Tooltip } from "@mui/material";
 import { CommonValueViewProps } from "./common";
 import S3Icon from "./s3.png";
+import { useRef } from "react";
 
 export function S3LocationValueView(props: CommonValueViewProps) {
   const { valueSummary } = props;
@@ -11,7 +12,7 @@ export function S3LocationValueView(props: CommonValueViewProps) {
 
   return <S3Button region={bucketSummary.region} bucket={bucketSummary.name} location={values.location} />;
 }
-  
+
 export function S3BucketValueView(props: CommonValueViewProps) {
   const { valueSummary } = props;
   const { values } = valueSummary;
@@ -19,7 +20,7 @@ export function S3BucketValueView(props: CommonValueViewProps) {
   return <S3Button region={values.region} bucket={values.name} />;
 }
 
-function S3Button(props: {region?: string, bucket: string, location?: string}) {
+function S3Button(props: { region?: string, bucket: string, location?: string }) {
   const { region, bucket, location } = props;
 
   let s3URI = "s3://" + bucket;
@@ -36,17 +37,46 @@ function S3Button(props: {region?: string, bucket: string, location?: string}) {
     href.searchParams.append("prefix", location);
   }
 
-  return <>
-    <Tooltip title="View in AWS console">
-      <Button
-        href={href.href}
-        variant="outlined"
-        target="blank"
-        endIcon={<OpenInNew />}
-      >
-        <img src={S3Icon} width="20px" style={{paddingRight: "5px"}} alt="S3 icon"/>
-        {s3URI}
-      </Button>
-    </Tooltip>
-  </>;
+  // The following code is to allow the user to select the text in the button
+  // without triggering the link.
+  const isSelectingText = useRef(false);
+
+  const onMouseDown = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    isSelectingText.current = false;
+  }
+
+  const onMouseUp = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    if (isSelectingText.current) {
+      e.preventDefault();
+    }
+  }
+
+  const onClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    if (isSelectingText.current) {
+      e.preventDefault();
+    }
+  }
+
+  const onMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (e.buttons === 1) {
+      isSelectingText.current = true;
+    }
+  }
+
+  return <Tooltip title="View in AWS console">
+    <Button
+      href={href.href}
+      variant="outlined"
+      target="blank"
+      endIcon={<OpenInNew />}
+      draggable={false}
+      style={{ userSelect: "text" }}
+      onClick={onClick}
+      onMouseDown={onMouseDown}
+      onMouseUp={onMouseUp}
+    >
+      <img src={S3Icon} width="20px" style={{ paddingRight: "5px" }} draggable="false" alt="" />
+      <div onMouseMove={onMouseMove} style={{ cursor: 'text' }} >{s3URI}</div>
+    </Button>
+  </Tooltip>;
 }
