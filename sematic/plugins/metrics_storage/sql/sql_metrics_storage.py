c0c9b4d7b65fdb4143a8192ceb3635d4123ae802
Emmanuel Turlay <emmanuel@sematic.ai>
2023-04-14 16:03:45 -0700

    Fix build

--- sematic/plugins/metrics_storage/sql/sql_metrics_storage.py
+++ sematic/plugins/metrics_storage/sql/sql_metrics_storage.py
@@ -138,7 +138,8 @@ class SQLMetricsStorage(AbstractMetricsStorage, AbstractPlugin):
             func.sum(MetricValue.value),
             func.count(MetricValue.value),
         ]
-    n_basic_fields = len(select_fields)
+
+        n_basic_fields = len(select_fields)
 
         n_basic_fields = len(select_fields)
 
