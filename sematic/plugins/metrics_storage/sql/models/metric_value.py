eb9e6262e873469dbd08c9f5beed1b8c75fe95bf
Emmanuel Turlay <emmanuel@sematic.ai>
2023-04-20 15:01:00 -0700

    PR comments

--- sematic/plugins/metrics_storage/sql/models/metric_value.py
+++ sematic/plugins/metrics_storage/sql/models/metric_value.py
@@ -42,12 +42,3 @@ class MetricValue(Base):
     created_at: datetime.datetime = Column(
         types.DateTime(), nullable=False, default=datetime.datetime.utcnow
     )
-
-    @validates("metric_time")
-    def _validate_metric_time(
-        self, _, metric_time: datetime.datetime
-    ) -> datetime.datetime:
-        if metric_time is None:
-            raise ValueError()
-
-        return metric_time
