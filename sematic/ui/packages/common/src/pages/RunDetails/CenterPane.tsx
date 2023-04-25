8e2a99928d760184525af80d84891fd5ec1a69c6
Chance An <anchangsi@gmail.com>
2023-04-25 11:25:44 -0700

    Implements the static Run details page center column (#756)
    ![capture1](https://user-images.githubusercontent.com/1046489/233213673-744ca091-7c2f-430e-848c-e6fd1cefd4cf.gif)
    
    
    Please use this link to try out the latest experience:
    
    
    https://story.dev-usw2-sematic0.sematic.cloud/iframe.html?args=&id=sematic-page--page&viewMode=story

---
+++ sematic/ui/packages/common/src/pages/RunDetails/CenterPane.tsx
@@ -0,0 +1,11 @@
+import FunctionSection from 'src/pages/RunDetails/FunctionSection';
+import RunTabs from 'src/pages/RunDetails/RunTabs';
+
+const CenterPane = () => {
+    return <>
+        <FunctionSection />
+        <RunTabs />
+    </>;
+}
+
+export default CenterPane;
