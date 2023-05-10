eeb3236a60c98b25971451049dc4254646ca019c
augray <augray@users.noreply.github.com>
2023-05-10 16:11:19 -0700

    Update sematic/ui/packages/main/src/components/Health.tsx
    Co-authored-by: tscurtu <tudor@sematic.dev>

--- sematic/ui/packages/main/src/components/Health.tsx
+++ sematic/ui/packages/main/src/components/Health.tsx
@@ -50,7 +50,7 @@ function useFetchHealth(): string | null {
       message = value.db.message;
     }
   } else if(!!error){
-    message = "Unable to verify even basic Sematic API access.";
+    message = "Unable to connect to the Sematic API.";
   }
   return message;
 }
