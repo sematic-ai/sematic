4ae0cf5755767f2878230df6214e6ed34cdd5c67
chance-sematic <133257643+chance-sematic@users.noreply.github.com>
2023-06-14 10:38:28 -0700

    Dagview new version (#885)
    Implements the new edition of the DAG view
    
    1. Adopts [ReactFlow](https://reactflow.dev/) library.
    2. Unlike the old version, in the new version we don't display
    artifacts.
    3. The dummy intermediate run nodes ("_make_list", "_make_tuple",
    "_getitem") are now removed, with their inputs and outputs merged
    transitively (which means the edges going through linked dummy nodes are
    eliminated and combined as one)
    4. This version resolves the flickering issue when the graph is being
    updated.
    
    This implementation sticks with the below Figma design:
    
    <img width="475" alt="image"
    src="https://github.com/sematic-ai/sematic/assets/133257643/a09bd917-d835-428d-b187-148e5ccf6722">
    
    
    Screenshot:
    
    <img width="1609" alt="image"
    src="https://github.com/sematic-ai/sematic/assets/133257643/393144e4-cf25-41ac-bbeb-373659a379eb">
    
    Closes #884
    
    Co-authored-by: Chance An <anchangsi@gmail.com>

--- sematic/ui/packages/common/src/pages/RunDetails/CenterPane.tsx
+++ sematic/ui/packages/common/src/pages/RunDetails/CenterPane.tsx
@@ -1,11 +1,21 @@
+import { useRunDetailsSelectionContext } from "src/context/RunDetailsSelectionContext";
 import FunctionSection from "src/pages/RunDetails/FunctionSection";
 import RunTabs from "src/pages/RunDetails/RunTabs";
+import { useMemo } from "react";
+import ReactFlowDag from "src/pages/RunDetails/dag/ReactFlowDag";
 
 const CenterPane = () => {
-    return <>
-        <FunctionSection />
-        <RunTabs />
-    </>;
+    const { selectedPanel } = useRunDetailsSelectionContext();
+
+    return useMemo(() => {
+        if (selectedPanel === "dag") {
+            return <ReactFlowDag />;
+        }
+        return <>
+            <FunctionSection />
+            <RunTabs />
+        </>;
+    }, [selectedPanel]);
 }
 
 export default CenterPane;
