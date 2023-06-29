4ee91ded96ffc603ad9ff92f62bb33bbd272d334
augray <augray@users.noreply.github.com>
2023-06-29 13:17:04 -0400

    Summarization fine-tuning example (#877)
    See the example README for a description of the example. So far, this
    only contains variants of the "Flan" model, but others will be added.
    
    Sample Results
    ------------------
    
    [CNN article
    summarization](https://josh.dev-usw2-sematic0.sematic.cloud/runs/faae39c4d38b454cbffa40275b774025)
    
    [Amazon review
    headlining](https://josh.dev-usw2-sematic0.sematic.cloud/runs/c4371aef921b4cea9f2a6aab4c18a96f)
    
    Note that the name of the pipeline and its path have changed since some
    of these screenshots were taken and the above full-trainings were
    created.
    
    
    ![cnnScreenshot](https://github.com/sematic-ai/sematic/assets/7181589/5c8e3d75-9252-4938-b4d1-b16182649e98)
    
    ![cnnMetrics](https://github.com/sematic-ai/sematic/assets/7181589/02108289-5b16-4105-a4dc-6e3facb686b1)
    
    ![cnnOutput](https://github.com/sematic-ai/sematic/assets/7181589/fa6e8831-31b8-46ab-bd51-7059c558a0c3)
    
    ![cnnDatasetInput](https://github.com/sematic-ai/sematic/assets/7181589/c16cfc50-c1ba-41a6-bb03-57276f949eed)
    
    ---------
    
    Co-authored-by: Josh Bauer <josh@sematic.dev>

---
+++ sematic/examples/summarization_finetune/__main__.py
@@ -0,0 +1,242 @@
+""" Launch script for the summarization model finetuning
+isort:skip_file
+"""
+# Standard Library
+import argparse
+from dataclasses import replace
+from typing import Optional, Tuple
+
+# Third-party
+from sematic import torch_patch  # noqa: F401
+from huggingface_hub import login
+from peft import LoraConfig, PeftType
+
+# Sematic
+from sematic import LocalResolver
+from sematic.examples.summarization_finetune.pipeline import (
+    DatasetConfig,
+    ModelSize,
+    TrainingConfig,
+    pipeline,
+)
+from sematic.examples.summarization_finetune.train_eval import (
+    HuggingFaceDatasetReference,
+    HuggingFaceModelReference,
+    TrainingArguments,
+)
+
+
+LORA_CONFIG = LoraConfig(
+    r=16,
+    lora_alpha=1,
+    target_modules=["q", "v"],
+    lora_dropout=0.05,
+    bias="none",
+    peft_type=PeftType.LORA,
+    task_type="SEQ_2_SEQ_LM",
+    base_model_name_or_path="",
+)
+
+TRAINING_ARGS = TrainingArguments(
+    "temp",
+    evaluation_strategy="epoch",
+    learning_rate=1e-3,
+    gradient_accumulation_steps=1,
+    auto_find_batch_size=True,
+    num_train_epochs=1,
+    save_steps=100,
+    save_total_limit=8,
+    logging_steps=100,
+)
+
+TRAINING_CONFIG = TrainingConfig(
+    model_size=ModelSize.base,
+    lora_config=LORA_CONFIG,
+    training_arguments=TRAINING_ARGS,
+    storage_directory="~/tmp/summarization-tuned-model",
+)
+
+DATASET_CONFIG = DatasetConfig(
+    max_input_length=1024,
+    max_output_length=256,
+    max_train_samples=None,
+    max_test_samples=None,
+    dataset_ref=HuggingFaceDatasetReference.from_string("cnn_dailymail:1.0.0"),
+    text_column="article",
+    summary_column="highlights",
+)
+
+
+def main():
+    training_config, dataset_config, export_reference = parse_args()
+    resolver = LocalResolver()
+    future = pipeline(training_config, dataset_config, export_reference).set(
+        name="Summarization Fine-Tuning",
+        tags=[f"model-size:{training_config.model_size.name}"],
+    )
+    resolver.resolve(future)
+
+
+def parse_args() -> Tuple[
+    TrainingConfig, DatasetConfig, Optional[HuggingFaceModelReference]
+]:
+    parser = argparse.ArgumentParser("Hugging Face Summarization Example")
+    size_options = ", ".join([size.name for size in ModelSize])
+    parser.add_argument(
+        "--model-size",
+        type=lambda size: ModelSize[size],
+        default=TRAINING_CONFIG.model_size.name,
+        help=(
+            f"Select a model size (see https://huggingface.co/google/flan-t5-base)"
+            f". Options:  {size_options}."
+        ),
+    )
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=1,
+        help="The number of epochs to train on.",
+    ),
+    parser.add_argument(
+        "--learning-rate",
+        type=float,
+        default=TRAINING_ARGS.learning_rate,
+        help="The learning rate for the training.",
+    )
+    parser.add_argument(
+        "--logging-steps",
+        type=int,
+        default=TRAINING_ARGS.logging_steps,
+        help="Number of steps between logging metrics during training.",
+    )
+    parser.add_argument(
+        "--max-train-samples",
+        type=int,
+        default=DATASET_CONFIG.max_train_samples,
+        help="Maximum number of samples in a single training epoch.",
+    )
+    parser.add_argument(
+        "--max-test-samples",
+        type=int,
+        default=DATASET_CONFIG.max_test_samples,
+        help="Maximum number of samples in testing.",
+    )
+    parser.add_argument(
+        "--max-input-length",
+        type=int,
+        default=DATASET_CONFIG.max_input_length,
+        help="Maximum number of tokens in the input prompt.",
+    )
+    parser.add_argument(
+        "--max-output-length",
+        type=int,
+        default=DATASET_CONFIG.max_output_length,
+        help="Maximum number of output tokens in response to the prompt.",
+    )
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        default=DATASET_CONFIG.dataset_ref.to_string(),
+        help=(
+            "The Hugging Face dataset to use. Should be structured as: "
+            "<owner>/<repo>:<subset>. For datasets owned by HuggingFace, omit <owner>/ "
+            "Ex: cnn_dailymail:1.0.0 for https://huggingface.co/datasets/cnn_dailymail"
+        ),
+    )
+    parser.add_argument(
+        "--text-column",
+        type=str,
+        default=DATASET_CONFIG.text_column,
+        help=(
+            "The name of the column in the source dataset holding the text to summarize"
+        ),
+    )
+    parser.add_argument(
+        "--summary-column",
+        type=str,
+        default=DATASET_CONFIG.summary_column,
+        help=(
+            "The name of the column in the source dataset holding the summary of "
+            "the text."
+        ),
+    )
+    parser.add_argument(
+        "--model-export-repo",
+        type=str,
+        default=None,
+        help=(
+            "A huggingface repo to export the model to. "
+            "Should be provided as <repo-owner>/<repo name>."
+        ),
+    )
+    parser.add_argument(
+        "--lora-r",
+        type=int,
+        default=LORA_CONFIG.r,
+        help="The number of dimensions for the LoRA matrix.",
+    )
+    parser.add_argument(
+        "--lora-alpha",
+        type=int,
+        default=LORA_CONFIG.lora_alpha,
+        help="The scaling factor for the LoRA trained weights.",
+    )
+    parser.add_argument(
+        "--lora-dropout",
+        type=float,
+        default=LORA_CONFIG.lora_dropout,
+        help="The dropout probability for LoRA layers.",
+    )
+    parser.add_argument(
+        "--login",
+        default=False,
+        action="store_true",
+        help=(
+            "Whether or not to prompt for HuggingFace login. "
+            "Should be done on first script execution."
+        ),
+    )
+    args = parser.parse_args()
+
+    training_config = replace(
+        TRAINING_CONFIG,
+        model_size=args.model_size,
+        training_arguments=replace(
+            TRAINING_ARGS,
+            learning_rate=args.learning_rate,
+            num_train_epochs=args.epochs,
+            logging_steps=args.logging_steps,
+        ),
+        lora_config=replace(
+            LORA_CONFIG,
+            r=args.lora_r,
+            lora_alpha=args.lora_alpha,
+            lora_dropout=args.lora_dropout,
+        ),
+    )
+
+    dataset_config = replace(
+        DATASET_CONFIG,
+        max_train_samples=args.max_train_samples,
+        max_test_samples=args.max_test_samples,
+        max_input_length=args.max_input_length,
+        max_output_length=args.max_output_length,
+        dataset_ref=HuggingFaceDatasetReference.from_string(args.dataset),
+        text_column=args.text_column,
+        summary_column=args.summary_column,
+    )
+
+    export_model_reference = None
+    if args.model_export_repo is not None:
+        export_model_reference = HuggingFaceModelReference.from_string(
+            args.model_export_repo
+        )
+
+    if args.login:
+        login()
+
+    return training_config, dataset_config, export_model_reference
+
+
+if __name__ == "__main__":
+    main()
