c273acfda8285c2874f2d045ef94355df7c810e8
Josh Bauer <josh@sematic.dev>
2023-06-29 18:07:12 +0000

    add GPT-J support

--- sematic/examples/summarization_finetune/train_eval.py
+++ sematic/examples/summarization_finetune/train_eval.py
@@ -1,12 +1,14 @@
 # Standard Library
+import time
 from dataclasses import asdict, dataclass, replace
 from enum import Enum, unique
-from typing import List, Optional, Tuple
+from typing import Any, Dict, List, Optional, Tuple, Union
 
 # Third-party
 from datasets import Dataset, load_dataset
 from peft import LoraConfig, PeftModelForSeq2SeqLM, get_peft_model
 from transformers import (
+    AutoModelForCausalLM,
     AutoModelForSeq2SeqLM,
     AutoTokenizer,
     PreTrainedTokenizerBase,
@@ -15,14 +17,24 @@ from transformers import (
 )
 from transformers import TrainingArguments as HfTrainingArguments
 
+try:
+    # Third-party
+    from trl import SFTTrainer
+except ImportError:
+    print("Training with SFTTrainer not supported. Please install trl.")
+    SFTTrainer = Trainer
+
 # Sematic
 # Metrics logging is only available for Sematic ee users.
+# from sematic.ee.metrics import MetricScope, log_metric
 # from sematic.ee.metrics import MetricScope, log_metric
 from sematic.types import (
     HuggingFaceDatasetReference,
     HuggingFaceModelReference,
     PromptResponse,
 )
+
+_SUMMARY_START_INDICATOR = "**Summary**: "
 
 
 def log_metric(name, value):
@@ -40,13 +52,71 @@ class LogMetricsCallback(TrainerCallback):
             log_metric(metric, value)
 
 
+class SftPeftCallback(TrainerCallback):
+    def __init__(self):
+        self.model = None
+        super().__init__()
+
+    def on_save(self, args, state, control, **kwargs):
+        self.model = kwargs["model"]
+
+
 @unique
-class ModelSize(Enum):
-    small = "small"
-    base = "base"
-    large = "large"
-    xl = "xl"
-    xxl = "xxl"
+class ModelType(Enum):
+    seq_to_seq = "seq_to_seq"
+    causal = "causal"
+
+
+@unique
+class ModelSelection(Enum):
+    flan_small = "flan_small"
+    flan_base = "flan_base"
+    flan_large = "flan_large"
+    flan_xl = "flan_xl"
+    flan_xxl = "flan_xxl"
+    gpt_j_6b = "gpt_j_6b"
+
+    @classmethod
+    def from_model_reference(cls, ref: HuggingFaceModelReference) -> "ModelSelection":
+        if "flan" in ref.repo:
+            return ModelSelection[ref.repo.replace("flan-t5-", "flan_")]
+        else:
+            return ModelSelection[ref.repo.replace("-", "_")]
+
+    def is_flan(self) -> bool:
+        return self in {
+            ModelSelection.flan_small,
+            ModelSelection.flan_base,
+            ModelSelection.flan_large,
+            ModelSelection.flan_xl,
+            ModelSelection.flan_xxl,
+        }
+
+
+@dataclass(frozen=True)
+class ModelProperties:
+    model_type: ModelType
+    pad_token: Optional[str] = None
+    load_in_8bit: bool = False
+    device_map: Optional[Union[str, Dict[str, Any]]] = "auto"
+
+
+_FLAN_PROPS = ModelProperties(model_type=ModelType.seq_to_seq)
+_GPTJ_PROPS = ModelProperties(
+    model_type=ModelType.causal,
+    pad_token="eos_token",
+    device_map=None,
+    load_in_8bit=True,
+)
+
+_MODEL_PROPERTIES = {
+    ModelSelection.flan_small: _FLAN_PROPS,
+    ModelSelection.flan_base: _FLAN_PROPS,
+    ModelSelection.flan_large: _FLAN_PROPS,
+    ModelSelection.flan_xl: _FLAN_PROPS,
+    ModelSelection.flan_xxl: _FLAN_PROPS,
+    ModelSelection.gpt_j_6b: _GPTJ_PROPS,
+}
 
 
 # works around: https://github.com/huggingface/transformers/issues/23958
@@ -58,7 +128,7 @@ class TrainingArguments:
     gradient_accumulation_steps: int
     auto_find_batch_size: bool
     num_train_epochs: int
-    save_steps: int
+    save_strategy: str
     save_total_limit: int
     logging_steps: int
 
@@ -68,7 +138,7 @@ class TrainingArguments:
 
 @dataclass
 class TrainingConfig:
-    model_size: ModelSize
+    model_selection: ModelSelection
     lora_config: LoraConfig
     training_arguments: TrainingArguments
     storage_directory: str
@@ -91,24 +161,57 @@ class EvaluationResults:
 
 
 def load_model(model_name):
-    model = AutoModelForSeq2SeqLM.from_pretrained(
-        model_name,
-        device_map="auto",
+    model_props = _MODEL_PROPERTIES[
+        ModelSelection.from_model_reference(
+            HuggingFaceModelReference.from_string(model_name)
+        )
+    ]
+    auto_model_type = (
+        AutoModelForSeq2SeqLM
+        if model_props.model_type is ModelType.seq_to_seq
+        else AutoModelForCausalLM
     )
+
+    model = auto_model_type.from_pretrained(
+        model_name,
+        device_map=model_props.device_map,
+        load_in_8bit=model_props.load_in_8bit,
+    )
+
     return model
 
 
 def load_tokenizer(model_name) -> PreTrainedTokenizerBase:
-    return AutoTokenizer.from_pretrained(model_name, device_map="auto")
+    model_props = _MODEL_PROPERTIES[
+        ModelSelection.from_model_reference(
+            HuggingFaceModelReference.from_string(model_name)
+        )
+    ]
+    tokenizer = AutoTokenizer.from_pretrained(model_name, device_map="auto")
+    if model_props.pad_token is not None:
+        tokenizer.pad_token = getattr(tokenizer, model_props.pad_token)
+    return tokenizer
 
 
-def _docs_preprocess_function(examples, tokenizer, dataset_config):
+def _causal_preprocess_function(examples, tokenizer, dataset_config):
+    text_column = dataset_config.text_column
+    label_column = dataset_config.summary_column
+
+    inputs = [
+        f"**Please summarize**: {ctx}. " f"{_SUMMARY_START_INDICATOR}{summary} **End**"
+        for ctx, summary in zip(examples[text_column], examples[label_column])
+    ]
+
+    return {"text": inputs}
+
+
+def _seq_2_seq_preprocess_function(examples, tokenizer, dataset_config):
     text_column = dataset_config.text_column
     label_column = dataset_config.summary_column
     max_length = dataset_config.max_input_length
     output_token_max_length = dataset_config.max_output_length
     inputs = [
-        f"<Please summarize>: {ctx}. <Summary>: " for ctx in examples[text_column]
+        f"**Please summarize**: {ctx}. **Summary**: " for ctx in examples[text_column]
     ]
     targets = [target for target in examples[label_column]]
     model_inputs = tokenizer(
@@ -134,6 +237,7 @@ def _docs_preprocess_function(examples, tokenizer, dataset_config):
 def prepare_data(
     dataset_config: DatasetConfig,
     tokenizer: PreTrainedTokenizerBase,
+    model_type: ModelType,
 ):
     dataset = load_dataset(
         dataset_config.dataset_ref.to_string(full_dataset=True),
@@ -165,8 +269,18 @@ def prepare_data(
             range(dataset_config.max_test_samples)
         )
 
+    def seq_2_seq_preprocessor(example):
+        return _seq_2_seq_preprocess_function(example, tokenizer, dataset_config)
+
+    def causal_preprocessor(example):
+        return _causal_preprocess_function(example, tokenizer, dataset_config)
+
     processed_datasets = dataset.map(
-        lambda example: _docs_preprocess_function(example, tokenizer, dataset_config),
+        (
+            seq_2_seq_preprocessor
+            if model_type is ModelType.seq_to_seq
+            else causal_preprocessor
+        ),
         batched=True,
         num_proc=1,
         remove_columns=dataset["train"].column_names,
@@ -175,6 +289,7 @@ def prepare_data(
     )
     train_dataset = processed_datasets["train"]
     eval_dataset = processed_datasets["validation"]
+
     return train_dataset, eval_dataset
 
 
@@ -183,40 +298,131 @@ def train(
     train_config: TrainingConfig,
     train_data: Dataset,
     eval_data: Dataset,
+    tokenizer: PreTrainedTokenizerBase,
 ) -> PeftModelForSeq2SeqLM:
+    model_properties = _MODEL_PROPERTIES[
+        ModelSelection.from_model_reference(
+            HuggingFaceModelReference.from_string(model_name)
+        )
+    ]
+    training_args = train_config.training_arguments.to_hugging_face()
     model = load_model(model_name)
-    model = get_peft_model(model, train_config.lora_config)
-    trainer = Trainer(
-        model=model,
-        args=train_config.training_arguments.to_hugging_face(),
-        train_dataset=train_data,
-        eval_dataset=eval_data,
-        callbacks=[LogMetricsCallback()],
-    )
-    model.config.use_cache = False
-    trainer.train()
+    if model_properties.model_type is ModelType.causal:
+        saver = SftPeftCallback()
+        trainer = SFTTrainer(
+            model=model,
+            args=training_args,
+            peft_config=train_config.lora_config,
+            train_dataset=train_data,
+            eval_dataset=eval_data,
+            tokenizer=tokenizer,
+            packing=False,
+            dataset_text_field="text",
+            callbacks=[LogMetricsCallback(), saver],
+        )
+        model.config.use_cache = False
+        trainer.train()
+        model = saver.model
+    else:
+        model = get_peft_model(model, train_config.lora_config)
+        trainer = Trainer(
+            model=model,
+            args=training_args,
+            train_dataset=train_data,
+            eval_dataset=eval_data,
+            callbacks=[LogMetricsCallback()],
+        )
+        model.config.use_cache = False
+        trainer.train()
+
     return model
+
+
+def extract_prompt(text: str, max_tokens: int) -> str:
+    # We need to make sure the prompt to summarize
+    # doesn't get truncated out. Rule of thumb is
+    # that there are roughly 4 English chars per token
+    # on average. So the number of chars should be
+    # truncated to be a little less than 4x the max
+    # number of tokens, with enough space for
+    # the Summarize prompt. We can go with 3.5x to
+    # provide some margin for error.
+    def trim_context(context):
+        truncated = context[: int(3.5 * max_tokens) - len(_SUMMARY_START_INDICATOR)]
+        if truncated == context:
+            return context
+
+        return truncated
+
+    summary_start_start = text.find(_SUMMARY_START_INDICATOR)
+    truncated = trim_context(text[:summary_start_start])
+    result = f"{truncated} {_SUMMARY_START_INDICATOR}"
+    return result
+
+
+def _run_eval_tokenizer(examples, tokenizer, max_tokens):
+    inputs = examples["text"]
+    model_inputs = tokenizer(
+        inputs,
+        max_length=max_tokens,
+        padding="max_length",
+        truncation=True,
+        return_tensors="pt",
+    )
+    return model_inputs
 
 
 def evaluate(
     model: PeftModelForSeq2SeqLM,
     eval_dataset: Dataset,
     tokenizer: PreTrainedTokenizerBase,
+    model_type: ModelType,
+    dataset_config: DatasetConfig,
 ) -> EvaluationResults:
     model.eval()
     results: List[Tuple[str, str]] = []
     model.config.use_cache = True
+    if model_type is ModelType.causal:
+        eval_dataset = eval_dataset.map(
+            lambda example: {
+                "text": extract_prompt(example["text"], dataset_config.max_input_length)
+            },
+            batched=False,
+            num_proc=1,
+            desc="Extracting eval prompts",
+        )
+        eval_dataset = eval_dataset.map(
+            lambda examples: _run_eval_tokenizer(
+                examples, tokenizer, dataset_config.max_input_length
+            ),
+            batched=True,
+            num_proc=1,
+            remove_columns=["text"],
+            desc="Tokenizing eval prompts",
+        )
     eval_dataset.set_format(type="torch", columns=["input_ids", "attention_mask"])
+
+    started = time.time()
     for i, row in enumerate(eval_dataset.iter(batch_size=1)):
-        print(f"Eval sample {i}")
+        seconds_since_start = int(time.time() - started)
+        print(f"Eval sample {i} (after {seconds_since_start} s)")
         eval_tokens = row["input_ids"]
         input_text = tokenizer.batch_decode(
             eval_tokens.detach().cpu().numpy(), skip_special_tokens=True
         )
-        output_tokens = model.generate(input_ids=eval_tokens, max_new_tokens=500)
+        output_tokens = model.generate(
+            input_ids=eval_tokens,
+            max_new_tokens=dataset_config.max_output_length,
+            pad_token_id=tokenizer.pad_token_id,
+        )
         output_text = tokenizer.batch_decode(
             output_tokens.detach().cpu().numpy(), skip_special_tokens=True
         )
+        if model_type is ModelType.causal:
+            # Causal ML models extend the input. In our case, we want to
+            # consider the "response" to only be the new part, not counting
+            # the input.
+            output_text = [output_text[0].replace(input_text[0], "", 1)]
         results.append(
             PromptResponse(sanitize(input_text[0]), sanitize(output_text[0]))
         )
