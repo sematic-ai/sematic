18b06827982d053aa87e7afec86170ce32a50f6d
augray <augray@users.noreply.github.com>
2023-06-29 13:17:04 -0400

    Summarization fine-tuning example (#877)
    See the example README for a description of the example. So far, this
    only contains variants of the "Flan" model, but others will be added.
    
    Sample Results
    ------------------
    
    [CNN article
    summarization](https://josh.dev-usw2-sematic0.sematic.cloud/runs/faae39c4d38b454cbffa40275b774025)
    
    [Amazon review
    headlining](https://josh.dev-usw2-sematic0.sematic.cloud/runs/c4371aef921b4cea9f2a6aab4c18a96f)
    
    Note that the name of the pipeline and its path have changed since some
    of these screenshots were taken and the above full-trainings were
    created.
    
    
    ![cnnScreenshot](https://github.com/sematic-ai/sematic/assets/7181589/5c8e3d75-9252-4938-b4d1-b16182649e98)
    
    ![cnnMetrics](https://github.com/sematic-ai/sematic/assets/7181589/02108289-5b16-4105-a4dc-6e3facb686b1)
    
    ![cnnOutput](https://github.com/sematic-ai/sematic/assets/7181589/fa6e8831-31b8-46ab-bd51-7059c558a0c3)
    
    ![cnnDatasetInput](https://github.com/sematic-ai/sematic/assets/7181589/c16cfc50-c1ba-41a6-bb03-57276f949eed)
    
    ---------
    
    Co-authored-by: Josh Bauer <josh@sematic.dev>

---
+++ sematic/examples/summarization_finetune/pipeline.py
@@ -0,0 +1,126 @@
+# Standard Library
+from dataclasses import dataclass
+from typing import Optional, Tuple
+
+# Third-party
+from datasets import Dataset
+from transformers import PreTrainedTokenizerBase
+
+# Sematic
+import sematic
+from sematic.examples.summarization_finetune.train_eval import (
+    DatasetConfig,
+    EvaluationResults,
+    HuggingFaceModelReference,
+    ModelSize,
+    TrainingConfig,
+    evaluate,
+    export_model,
+)
+from sematic.examples.summarization_finetune.train_eval import (
+    load_tokenizer as do_load_tokenizer,
+)
+from sematic.examples.summarization_finetune.train_eval import prepare_data
+from sematic.examples.summarization_finetune.train_eval import train as do_train
+from sematic.types import HuggingFaceStoredModel as StoredModel
+
+
+@dataclass(frozen=True)
+class ResultSummary:
+    source_model: HuggingFaceModelReference
+    trained_model: StoredModel
+    evaluation_results: EvaluationResults
+    pushed_model_reference: Optional[HuggingFaceModelReference]
+
+
+@sematic.func
+def pick_model(model_size: ModelSize) -> HuggingFaceModelReference:
+    return HuggingFaceModelReference(owner="google", repo=f"flan-t5-{model_size.value}")
+
+
+@sematic.func
+def load_tokenizer(
+    model_reference: HuggingFaceModelReference,
+) -> PreTrainedTokenizerBase:
+    return do_load_tokenizer(model_reference.to_string())
+
+
+@sematic.func
+def train(
+    model_reference: HuggingFaceModelReference,
+    training_config: TrainingConfig,
+    train_data: Dataset,
+    eval_data: Dataset,
+) -> StoredModel:
+    model = do_train(
+        model_reference.to_string(),
+        training_config,
+        train_data,
+        eval_data,
+    )
+    return StoredModel.store(model, training_config.storage_directory)
+
+
+@sematic.func
+def eval(
+    model: StoredModel,
+    eval_data: Dataset,
+    tokenizer: PreTrainedTokenizerBase,
+) -> EvaluationResults:
+    return evaluate(model.load(), eval_data, tokenizer)
+
+
+@sematic.func
+def prepare_datasets(
+    dataset_config: DatasetConfig,
+    tokenizer: PreTrainedTokenizerBase,
+) -> Tuple[Dataset, Dataset]:
+    train_dataset, test_dataset = prepare_data(dataset_config, tokenizer)
+    return train_dataset, test_dataset
+
+
+@sematic.func
+def export(
+    model: StoredModel,
+    push_model_ref: HuggingFaceModelReference,
+) -> HuggingFaceModelReference:
+    return export_model(model.load(), push_model_ref)
+
+
+@sematic.func
+def summarize(
+    source_model: HuggingFaceModelReference,
+    trained_model: StoredModel,
+    evaluation_results: EvaluationResults,
+    pushed_model_reference: Optional[HuggingFaceModelReference],
+) -> ResultSummary:
+    return ResultSummary(
+        source_model=source_model,
+        trained_model=trained_model,
+        evaluation_results=evaluation_results,
+        pushed_model_reference=pushed_model_reference,
+    )
+
+
+@sematic.func
+def pipeline(
+    training_config: TrainingConfig,
+    dataset_config: DatasetConfig,
+    export_reference: Optional[HuggingFaceModelReference],
+) -> ResultSummary:
+    model_ref = pick_model(training_config.model_size)
+    tokenizer = load_tokenizer(model_ref)
+    train_data, test_data = prepare_datasets(dataset_config, tokenizer)
+    model = train(model_ref, training_config, train_data, test_data)
+    eval_results = eval(model, test_data, tokenizer)
+
+    exported_model_reference = None
+    if export_reference is not None:
+        exported_model_reference = export(model, export_reference)
+
+    return summarize(
+        source_model=model_ref,
+        trained_model=model,
+        evaluation_results=eval_results,
+        pushed_model_reference=exported_model_reference,
+    )
