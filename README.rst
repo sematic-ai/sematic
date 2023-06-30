7b8dbb1b0d819333616395d74eb8f76c4ff87450
Sash Nagarkar <sashn45@gmail.com>
2023-06-30 10:14:05 -0700

    Release 0.31.2 (#949)
    Co-authored-by: augray <apps@joshbauer.email>

--- README.rst
+++ README.rst
@@ -6,8 +6,8 @@
 
 
 
-.. image:: https://img.shields.io/pypi/v/sematic/0.31.1?style=for-the-badge
-   :target: https://img.shields.io/pypi/v/sematic/0.31.1?style=for-the-badge
+.. image:: https://img.shields.io/pypi/v/sematic/0.31.2?style=for-the-badge
+   :target: https://img.shields.io/pypi/v/sematic/0.31.2?style=for-the-badge
    :alt: PyPI
 
 
--- sematic/versions.py
+++ sematic/versions.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 # the sdk. Should be bumped any time a release is made. Should be set
 # to whatever is the version after the most recent one in changelog.md,
 # as well as the version for the sematic wheel in wheel_constants.bzl
-CURRENT_VERSION = (0, 31, 1)
+CURRENT_VERSION = (0, 31, 2)
 
 # TO DEPRECATE
 # 0.32.0
