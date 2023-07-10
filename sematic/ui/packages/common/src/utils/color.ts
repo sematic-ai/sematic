5ab185d875ec0c8062bf406922d4d516cf51acff
chance-sematic <133257643+chance-sematic@users.noreply.github.com>
2023-07-10 15:54:13 -0700

    Display avatar alongside user names (#962)
    Preview:
    
    1. Style for the case when users have pre-defined profile images.
    <img width="1607" alt="image"
    src="https://github.com/sematic-ai/sematic/assets/133257643/06999e35-2e54-4199-9bbc-171a81da415f">
    
    2. Style for the case when users do not have profile images pre-defined.
    <img width="1608" alt="image"
    src="https://github.com/sematic-ai/sematic/assets/133257643/550ba925-3b06-40b6-aa65-77eb1ba56a80">

--- sematic/ui/packages/common/src/utils/color.ts
+++ sematic/ui/packages/common/src/utils/color.ts
@@ -10,3 +10,50 @@ const CHART_COLORS = [
 export function getChartColor(index: number): string {
     return CHART_COLORS[index % CHART_COLORS.length];
 }
+
+export const TAG_COLORS = [
+    {
+        color: "rgb(65, 36, 84)",
+        backgroundColor: "rgb(232, 222, 238)"
+    }, // purple
+    {
+        color: "rgb(24, 51, 71)",
+        backgroundColor: "rgb(91, 151, 189)"
+    }, // blue
+    {
+        color: "rgb(93, 23, 21)",
+        backgroundColor: "rgb(255, 226, 221)"
+    }, // pink
+    {
+        color: "rgb(93, 23, 21)",
+        backgroundColor: "rgba(238, 224, 218)"
+    }, // brown
+    {
+        color: "rgb(64, 44, 27)",
+        backgroundColor: "rgb(253, 236, 200)"
+    }, // yellow
+    {
+        color: "rgb(50, 48, 44)",
+        backgroundColor: "rgba(227, 226, 224, 0.5)"
+    }, // default
+    {
+        color: "rgb(145, 145, 142)",
+        backgroundColor: "rgb(227, 226, 224)"
+    }, // gray
+    {
+        color: "rgb(28, 56, 41)",
+        backgroundColor: "rgb(219, 237, 219)"
+    }, // green
+    {
+        color: "rgb(93, 23, 21)",
+        backgroundColor: "rgb(225, 111, 100)"
+    }, // red
+    {
+        color: "rgb(73, 41, 14)",
+        backgroundColor: "rgb(250, 222, 201)"
+    } // orange
+];
+
+export function getTagColor(anyNumber: number): typeof TAG_COLORS[0] {
+    return TAG_COLORS[anyNumber % TAG_COLORS.length];
+}
