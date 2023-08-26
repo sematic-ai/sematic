2b28f68c0f8057b2f24d134ad3cb0d8e6f63cd0a
augray <augray@users.noreply.github.com>
2023-08-25 21:33:02 -0400

    Improve `sematic version` CLI (#1045)
    The `sematic version` CLI was a bit misleading, as it checked the
    client's version and called it the server version. This PR changes it so
    that both the client and the server version are reported, along with the
    min version from the actual server.
    
    Testing
    -------
    
    ```
    $ sematic version
    Sematic client v0.34.0 is installed.
    The server is at version v0.34.0
    Client versions >= v0.30.0 are supported.
    Python v3.8.15 is running this binary.
    ```
    
    And with an unsupported server/client combo:
    
    ```
    $ sematic version
    Sematic client v0.34.0 is installed.
    The Sematic API client is at version 0.34.0, which is newer than the Sematic server's version of 0.33.0. Please downgrade your client to 0.33.0, or ask your server admin to upgrade to at least 0.34.0.
    ```

--- sematic/cli/version.py
+++ sematic/cli/version.py
@@ -5,8 +5,10 @@ import platform
 import click
 
 # Sematic
+from sematic import api_client
 from sematic.cli.cli import cli
-from sematic.versions import CURRENT_VERSION_STR, MIN_CLIENT_SERVER_SUPPORTS_STR
+from sematic.config.config import switch_env
+from sematic.versions import CURRENT_VERSION_STR, version_as_string
 
 
 @cli.command("version", short_help="Print version information.")
@@ -14,6 +16,16 @@ def version():
     """
     Print the Server, Client, and Python versions.
     """
-    click.echo(f"Sematic server v{CURRENT_VERSION_STR} is installed.")
-    click.echo(f"Client versions >= v{MIN_CLIENT_SERVER_SUPPORTS_STR} are supported.")
+    switch_env("user")
+    click.echo(f"Sematic client v{CURRENT_VERSION_STR} is installed.")
+    try:
+        server_version_metadata = api_client.validate_server_compatibility()
+        server_version = version_as_string(server_version_metadata["server"])
+        min_client_version = version_as_string(
+            server_version_metadata["min_client_supported"]
+        )
+        click.echo(f"The server is at version v{server_version}")
+        click.echo(f"Client versions >= v{min_client_version} are supported.")
+    except api_client.IncompatibleClientError as e:
+        click.echo(str(e))
     click.echo(f"Python v{platform.python_version()} is running this binary.")
